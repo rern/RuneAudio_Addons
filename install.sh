0a4193a41d4963138feb8f1206dc5cb2b73df03e
rern <rernrern@gmail.com>
2017-08-31 10:03:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,27 +16,39 @@ if grep -q 'Addons' /srv/http/app/templates/header.php; then
     exit
 fi
 
-wgetnc https://github.com/rern/RuneAudio/raw/master/Addons/addons.php
-wgetnc https://github.com/rern/RuneAudio/raw/master/Addons/runbash.sh
+wgetnc https://github.com/rern/RuneAudio/raw/master/Addons/addonbash.sh
+wgetnc https://github.com/rern/RuneAudio/raw/master/Addons/addondl.php
 wgetnc https://github.com/rern/RuneAudio/raw/master/Addons/uninstall_addo.sh -P /usr/local/bin
 chmod +x /srv/http/runbash.sh /usr/local/bin/uninstall_addo.sh
 
 sed -e '/poweroff-modal/ i\
-            <li><a href="/addons.php"><i class="fa fa-cubes"></i> Addons</a></li>
+            <li><a href id="addons"><i class="fa fa-cubes"></i> Addons</a></li>
 ' /srv/http/app/templates/header.php
 
+echo '
+$("#addons").click(function() {
+	$.get("addondl.php", function(data) {
+		if (data == 0) {
+			window.location.href = "addons.php";
+		} else {
+			alert("Addons server not reachable.");
+		}
+	});
+});
+' >> /srv/http/assets/js/runeui.js
+
+echo '$("#addons").click(function(){$.get("addondl.php",function(d){0==d?window.location.href="addons.php":alert("Addons server not reachable.")})});
+' >> /srv/http/assets/js/runeui.min.js
 # refresh #######################################
 echo -e "$bar Clear PHP OPcache ..."
-curl '127.0.0.1/clear'
+systemctl reload php-fpm
 echo
-
 if pgrep midori >/dev/null; then
 	killall midori
 	sleep 1
 	xinit &>/dev/null &
 	echo 'Local browser restarted.'
 fi
-
 timestop
 title -l = "$bar Addons menu installed successfully."
 echo 'Uninstall: uninstall_addo.sh'
