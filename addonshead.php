57b7992cc1f5851edcf4c6c927161d031f13a99d
rern <rernrern@gmail.com>
2017-09-01 16:04:59 +0700

    Create addonshead.php

---
+++ addonshead.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>RuneAudio - Addons</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no" />
+    <link rel="stylesheet" href="assets/css/runeui.css">
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+<style>
+.container {padding : 0 15px;}
+h1 {display: inline-block; width: calc(100% - 25px);}
+#close {vertical-align: 10px; width: 25px;}
+.boxed-group {padding: 10px 20px;}
+legend {margin-bottom: 5px;}
+.blue {color: #0095d8;}
+.btn {text-transform: capitalize;}
+p {color: #7795b4;}
+pre {color: #ddd;}
+.cc {color: #00ffff; background: #00ffff;}
+.ky {color: #000; background: #ffff00;}
+.ck {color: #00ffff;}
+.wr {background: #ff0000;}
+</style>
+</head>
+<body>
+
+<div class="container">
