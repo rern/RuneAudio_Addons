6a2f5a6919a2782af0ad4b301d5222f101bd55f2
rern <rernrern@gmail.com>
2017-09-01 16:35:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ $("#addons").click(function() {
 ' >> $footer
 
 echo '<?php
-$result = exec('wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/addons.php -O /srv/http/addons.php; echo $?');
+$result = exec('wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/addons.php -O /srv/http/addons.php; echo $?');
 if ($result == 0) exec('/usr/bin/sudo /usr/bin/systemctl reload php-fpm &; sleep 1');
 echo $result;
 ' > /srv/http/addondl.php
