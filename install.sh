72b321b31f5ce0b14546261ed633d9e18d03bb6e
rern <rernrern@gmail.com>
2017-09-01 16:27:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,23 +9,36 @@ rm $0
 # import heading function
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
-title -l = "$bar Install Addons menu ..."
-
 if grep -q 'Addons' /srv/http/app/templates/header.php; then
     echo -e "$info Already installed."
     exit
 fi
 
-wgetnc https://github.com/rern/RuneAudio_Addons/raw/master/uninstall_addo.sh -P /usr/local/bin
-chmod +x /srv/http/runbash.sh /usr/local/bin/uninstall_addo.sh
+# install RuneAudio Addons #######################################
+title -l = "$bar Install Addons menu ..."
+echo -e "$bar Get files ..."
+wgetnc https://github.com/rern/RuneAudio_Addons/archive/master.zip
 
-wgetnc https://github.com/rern/RuneAudio_Addons/raw/master/addonbash.php
-wgetnc https://github.com/rern/RuneAudio_Addons/raw/master/addonshead.php
+echo -e "$bar Install new files ..."
+mkdir -p /tmp/install
+bsdtar -xf master.zip --strip 1 -C /tmp/install
+rm master.zip /tmp/install/{.*,*.md,install.sh} &> /dev/null
+chmod 755 /tmp/install/root/* /tmp/install/usr/local/bin/uninstall*
 
+cp -r /tmp/install/* /
+rm -r /tmp/install
+
+# modify files #######################################
+echo -e "$bar Modify files ..."
+
+header=/srv/http/app/templates/header.php
+echo $header
 sed -e '/poweroff-modal/ i\
             <li><a href id="addons"><i class="fa fa-cubes"></i> Addons</a></li>
-' /srv/http/app/templates/header.php
+' $header
 
+footer=/srv/http/app/templates/footer.php
+echo $footer
 echo '
 <script><!--addons-->
 $("#addons").click(function() {
@@ -38,7 +51,7 @@ $("#addons").click(function() {
 	});
 });
 </script>
-' >> /srv/http/app/templates/footer.php
+' >> $footer
 
 echo '<?php
 $result = exec('wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/addons.php -O /srv/http/addons.php; echo $?');
