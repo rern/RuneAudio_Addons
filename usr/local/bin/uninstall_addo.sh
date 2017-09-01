a6935e5c91b6e8e1a3c6c360c6f48cdf4c124031
rern <rernrern@gmail.com>
2017-09-01 18:05:49 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -19,20 +19,17 @@ sed -i '/<!--addons/, /<\/script>/d' /srv/http/app/templates/footer.php
 echo -e "$bar Remove files ..."
 rm -rv /srv/http/{addonbash.php,addondl.php,addons.php,addonshead.php}
 
-# refresh #######################################
-echo -e "$bar Clear PHP OPcache ..."
-systemctl reload php-fpm
-echo
-
-if pgrep midori >/dev/null; then
-	killall midori
-	sleep 1
-	xinit &>/dev/null &
-	echo -e '\nLocal browser restarted.\n'
-fi
-
 redis-cli hdel addons main &> /dev/null
 
 title -l = "$bar Addons uninstalled successfully."
+
+# clear opcache and restart local browser #######################################
+systemctl reload php-fpm
+
+if pgrep midori > /dev/null; then
+	killall midori
+	sleep 1
+	xinit &> /dev/null &
+fi
 
 rm $0
