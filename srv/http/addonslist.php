c87ee846b95d9c63e546aa2c03a25a1c9c580054
rern <rernrern@gmail.com>
2017-09-03 22:16:34 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -161,7 +161,7 @@ $package = array(
 	'installurl'  => 'https://github.com/rern/RuneUI_password/raw/master/install.sh',
 );
 addonblock($package);
-$package = array(
+/*$package = array(
 	'title'       => 'Samba Upgrade',
 	'version'     => '20170901',
 	'alias'       => 'samb',
@@ -171,7 +171,7 @@ $package = array(
 	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/samba/installurl',
 	'option'      => 'Password for user root (Cancel for no password)',
 );
-addonblock($package);
+addonblock($package);*/
 $package = array(
 	'title'       => 'Transmission',
 	'version'     => '20170901',
