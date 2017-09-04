b2bf5f0943089dafc94479c2caac89ec873329e3
rern <rernrern@gmail.com>
2017-09-04 11:31:58 +0700

    Update addonshead.php

--- srv/http/addonshead.php
+++ srv/http/addonshead.php
@@ -9,66 +9,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="msapplication-tap-highlight" content="no" />
     <link rel="stylesheet" href="assets/css/runeui.css">
+    <link rel="stylesheet" href="assets/css/addons.css">
     <link rel="shortcut icon" href="assets/img/favicon.ico">
-<style>
-.container {
-	padding : 0 15px 20px;
-}
-h1 {
-	display: inline-block;
-	width: calc(100% - 25px);
-}
-#close {
-	vertical-align: 10px;
-	width: 25px;
-}
-.boxed-group {
-	padding: 10px 20px;
-}
-legend {
-	margin-bottom: 5px;
-}
-legend p {
-	display: inline-block;
-	font-size: 14px;
-	margin-left: 15px;
-}
-legend span {
-	margin-left: 8px;
-}
-legend span,
-pre {
-	color: #e0e7ee;
-}
-pre {
-	max-height: calc(100vh - 130px);
-	overflow: auto;
-}
-p {
-	color: #7795b4;
-}
-.btn {
-	text-transform: capitalize;
-}
-.blue {
-	color: #0095d8;
-}
-.cc {
-	color: #00ffff;
-	background: #00ffff;
-}
-.ky {
-	color: #000;
-	background: #ffff00;
-}
-.ck {
-	color: #00ffff;
-}
-.wr {
-	color: #ffffff;
-	background: #ff0000;
-}
-</style>
 </head>
 <body>
 
