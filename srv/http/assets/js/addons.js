89c179a598edc68eb34914b8a1794379d38397a5
rern <rernrern@gmail.com>
2017-09-04 20:44:19 +0700

    Create addons.js

---
+++ srv/http/assets/js/addons.js
@@ -0,0 +1,12 @@
+$( '#addons' ).click( function() {
+	$( '#loader' ).removeClass( 'hide' );
+	$.get( 'addondl.php', function( data ) {
+		if ( data == 1 ) {
+			window.location.href = 'addons.php';
+		} else {
+			alert( "Addons server cannot be reached.\n"
+        +"Try check and set date to current" );
+			$( '#loader' ).addClass( 'hide' );
+		}
+	});
+});
