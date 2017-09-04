bc8506e10ebb7a7d4733b30613b188a20d47d802
rern <rernrern@gmail.com>
2017-09-05 06:39:57 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+version=20170905
+
+# install.sh
+
+rm $0
+
+# import heading function
+wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
+
+if [[ ! -e /srv/http/addonbash.php ]]; then
+    echo -e "$info RuneAudio Addons not found."
+    exit
+fi
+
+title -l = "$bar Update RuneAudio Addons ..."
+
+# modify files #######################################
+echo -e "$bar Update files ..."
+
+file=/srv/http/assets/css/addons.css
+echo $file
+sed -i '/max-height: calc(100vh - 130px)/ i\
+    white-space: pre;
+' $file
+
+redis-cli hset addons addo $version &> /dev/null
+
+title -l = "$bar RuneAudio Addons updated successfully."
