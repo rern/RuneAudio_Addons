5964654ae221e49ddc73ec9439929cc06dbf0b74
rern <rernrern@gmail.com>
2017-09-05 20:28:02 +0700

    Update addons.js

--- srv/http/assets/js/addons.js
+++ srv/http/assets/js/addons.js
@@ -1,8 +1,10 @@
 $( '#addons' ).click( function() {
+	// fix path if click in other menu pages
+	var path = /\/.*\//.test( window.location.pathname ) ? '../../' : '';
 	$( '#loader' ).removeClass( 'hide' );
-	$.get( 'addondl.php', function( data ) {
+	$.get( path +'addondl.php', function( data ) {
 		if ( data == 1 ) {
-			window.location.href = 'addons.php';
+			window.location.href = path +'addons.php';
 		} else {
 			alert( "Addons server cannot be reached.\n"
 				+"Try check and set date to current." );
