b3b47a468b177e3aa3886e5e0906f5579576eadf
rern <rernrern@gmail.com>
2017-09-06 20:41:13 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -81,7 +81,7 @@ user input options:
 
 $addon = array(
 	'version'      => '20170906',
-	'title'        => 'Addons main',
+	'title'        => 'Addons Menu',
 	'maintainer'   => 'r e r n',
 	'description'  => 'This Addons main page.',
 	'thumbnail'    => 'https://github.com/rern/_assets/raw/master/RuneAudio_Addons/addonsthumb.png',
