30932887cdf803835bb0b9a64473c781f9be5da4
rern <rernrern@gmail.com>
2017-09-08 21:53:44 +0700

    Add files via upload

---
+++ srv/http/addonsdl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/addonslist.php -P /srv/http
+wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/changelog.md -P /srv/http
+
+sed -e '/^\s*$/ d
+' -e '1 {
+s/## //
+i\
+<?php
+s/^/$addonsversion = "/
+s/$/";/
+a\
+$log = \
+$addonsversion.'"'"' &nbsp; <a id="detail">changelog &#x25BC</a><br>\
+<div  id="message" style="display: none;">\
+	<ul>
+}
+' -e '/^- / {
+s/^- //
+s/^/	<li>/
+s|$|</li>|
+}
+' -e '/^## / {
+s/^## //
+i\
+	</ul>
+a\
+	<ul>
+}
+' -e '$ a\
+	</ul>\
+	<br>\
+</div>'"';"'
+' changelog.md > addonslog.php
+
+rm changelog.md
