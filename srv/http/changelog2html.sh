d6faa958521d06b8f06a4aa4fe56a380e33257d6
rern <rernrern@gmail.com>
2017-09-08 21:19:43 +0700

    Create changelog2html.sh

---
+++ srv/http/changelog2html.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# convert changelog.md to html
+
+#   ## abc -> abc
+#             <ul>
+#   - def  -> <li>def</li>
+#             </ul>
+#             <br>
+
+file=changelog.md
+
+sed -e '/^\s*$/ d
+' -e '1 s/## //
+' -e '1 i\
+<?php
+' -e '1 s/^/$addonsversion = "/
+' -e '1 s/$/";/
+' -e '1 a\
+$log = \
+$addonsversion.'"'"' &nbsp; <a id="detail">changelog &#x25BC</a><br>\
+<div  id="message" style="display: none;">\
+	<ul>
+' -e '/^- / s/^/<li>/
+' -e 's/^<li>- /	<li>/
+' -e '/^\s*<li>/ s|$|</li>|;
+' -e '/^## / i\
+	</ul>
+' -e '/^## / a\
+	<ul>
+' -e 's/^## //
+' -e '$ a\
+	</ul>\
+	<br>\
+</div>'"';"'
+' $file > changelog.php
