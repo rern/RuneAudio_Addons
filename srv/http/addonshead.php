f17a6ac0613e443b5e256e27b20b611994f7acb1
rern <rernrern@gmail.com>
2017-09-10 10:11:38 +0700

    Update addonshead.php

--- srv/http/addonshead.php
+++ srv/http/addonshead.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>RuneAudio - Addons</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="msapplication-tap-highlight" content="no" />
-    <link rel="stylesheet" href="assets/css/runeui.css">
-    <link rel="stylesheet" href="assets/css/addons.css">
-    <link rel="shortcut icon" href="assets/img/favicon.ico">
-    <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+	<meta charset="utf-8">
+	<title>RuneAudio - Addons</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="msapplication-tap-highlight" content="no" />
+	<link rel="stylesheet" href="assets/css/runeui.css">
+	<link rel="stylesheet" href="assets/css/addons.css">
+	<link rel="shortcut icon" href="assets/img/favicon.ico">
+	<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
 </head>
 <body>
 
-<!-- hide vertical scrollbar on desktop -->
+<!-- hide <pre> vertical scrollbar on desktop, must be here before 'flush' -->
 <script>
 var div = document.createElement('div');
 div.className = 'divscrollbar';
@@ -25,12 +25,12 @@ document.body.removeChild(div);
 
 if (scrollbarWidth !== 0) {
 	var css = '.hidescrollv {\n'
-				+'	width: 100%;\n'
-				+'	overflow: hidden;\n'
-				+'}\n'
-				+'pre {\n'
-				+'	width: calc(100% + '+ ( scrollbarWidth + 1 ) +'px);\n'
-				+'}';
+		+'	width: 100%;\n'
+		+'	overflow: hidden;\n'
+		+'}\n'
+		+'pre {\n'
+		+'	width: calc(100% + '+ ( scrollbarWidth + 1 ) +'px);\n'
+		+'}';
 	var style = document.createElement('style');
 	style.appendChild(document.createTextNode(css));
 	document.head.appendChild(style);
