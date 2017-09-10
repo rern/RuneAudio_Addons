c463c2a2429cb754e4ed5df1a5f102c90dd1af01
rern <rernrern@gmail.com>
2017-09-10 22:42:29 +0700

    Update addons.php

--- srv/http/addons.php
+++ srv/http/addons.php
@@ -42,7 +42,7 @@ function addonblock( $pkg ) {
 	if ( $GLOBALS[ 'version' ][ $alias ]) {
 		$check = '<i class="fa fa-check"></i> ';
 		if ( !isset( $pkg[ 'version' ] ) || $pkg[ 'version' ] == $GLOBALS[ 'version' ][ $alias ] ) {
-			// !!! <button> tag will fail on 'formtemp' submit, use <a> instead
+			// !!! mobile browsers: <button>s submit 'formtemp' with 'get' > 'failed', use <a> instead
 			$btnin = '<a class="btn btn-default disabled"><i class="fa fa-check"></i> '.$buttonlabel.'</a>';
 		} else {
 			$btnin = '<a cmd="'.$cmduninstall.'; [[ $? != 1 ]] && { '.$cmdinstall.'; }" class="btn btn-primary"><i class="fa fa-refresh"></i> Update</a>';
