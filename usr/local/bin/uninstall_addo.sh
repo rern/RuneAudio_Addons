c80680e788681cca234b4a12a5a6dbbe4e87d4bc
rern <rernrern@gmail.com>
2017-09-14 00:11:03 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
+# required variables
+alias=addo
+
 # import heading function
 wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; rm title.sh
 
-# check installed #######################################
-if [[ ! -e /usr/local/bin/uninstall_addo.sh ]]; then
-	echo -e "Addons Menu not found."
-	exit 1
-fi
-
-[[ $1 != u ]] && type=Uninstall || type=Update
-title -l = "$bar $type Addons Menu ..."
+uninstallstart $1
 
 # restore file
 sed -i '/id="addons"/ d' /srv/http/app/templates/header.php
@@ -20,18 +16,7 @@ sed -i '/addons.js/ d' /srv/http/app/templates/footer.php
 echo -e "$bar Remove files ..."
 rm -rv /srv/http/{addon*,assets/css/addons.css,assets/js/addons.js}
 
-redis-cli hdel addons addo &> /dev/null
-
-[[ $1 != u ]] && title -l = "$bar Addons Menu uninstalled successfully."
+uninstallfinish $1
 
 # clear opcache if run from terminal #######################################
-[[ -t 1 ]] && systemctl reload php-fpm
-
-# restart local browser #######################################
-if pgrep midori > /dev/null; then
-	killall midori
-	sleep 1
-	xinit &> /dev/null &
-fi
-
-rm $0
+[[ -t 1 ]] && clearcache
