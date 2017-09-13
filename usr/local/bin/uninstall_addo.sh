5cf0e6c83856830e5a469f61f7d8eafbbca4a62c
rern <rernrern@gmail.com>
2017-09-13 08:07:41 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -5,12 +5,12 @@ wget -qN https://github.com/rern/title_script/raw/master/title.sh; . title.sh; r
 
 # check installed #######################################
 if [[ ! -e /usr/local/bin/uninstall_addo.sh ]]; then
-	echo -e "Addons not found."
+	echo -e "Addons Menu not found."
 	exit 1
 fi
 
 [[ $1 != u ]] && type=Uninstall || type=Update
-title -l = "$bar $type Addons ..."
+title -l = "$bar $type Addons Menu ..."
 
 # restore file
 sed -i '/id="addons"/ d' /srv/http/app/templates/header.php
@@ -22,7 +22,7 @@ rm -rv /srv/http/{addon*,assets/css/addons.css,assets/js/addons.js}
 
 redis-cli hdel addons addo &> /dev/null
 
-[[ $1 != u ]] && title -l = "$bar Addons uninstalled successfully."
+[[ $1 != u ]] && title -l = "$bar Addons Menu uninstalled successfully."
 
 # clear opcache if run from terminal #######################################
 [[ -t 1 ]] && systemctl reload php-fpm
