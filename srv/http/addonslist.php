7740dc1ff2acf5375a5ba28c0d59c47e5706df11
rern <rernrern@gmail.com>
2017-09-14 22:11:18 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -21,8 +21,7 @@ array(
 	'thumbnail'    => 'https://github.com/rern/RuneAudio/raw/master/aria2/thumbaria.png',
 	'sourcecode'   => 'https://github.com/rern/RuneAudio/tree/master/aria2',
 	'installurl'   => 'https://github.com/rern/RuneAudio/raw/master/aria2/install.sh',
-	'option'       => '?Start &quot;Aria2&quot; on system startup;'
-			.'!Package &quot;glibc&quot; slow download may take some times.'
+	'option'       => '?Start &quot;Aria2&quot; on system startup?'
 ),*/
 array(
 	'alias'        => 'back',
