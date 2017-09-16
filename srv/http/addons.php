e8d93ce5a568ba95d9876b9727fc0b8c26214cd5
rern <rernrern@gmail.com>
2017-09-16 17:07:03 +0700

    Update addons.php

--- srv/http/addons.php
+++ srv/http/addons.php
@@ -14,45 +14,9 @@ $GLOBALS[ 'version' ] = $redis->hGetAll( 'addons' );
 $GLOBALS[ 'list' ] = '';
 $GLOBALS[ 'blocks' ] = '';
 
-// sort list
-function make_comparer() {
-	// https://stackoverflow.com/a/16788610
-	// Normalize criteria up front so that the comparer finds everything tidy
-	$criteria = func_get_args();
-	foreach ( $criteria as $index => $criterion ) {
-		$criteria[ $index ] = is_array( $criterion )
-			? array_pad( $criterion, 3, null )
-			: array( $criterion, SORT_ASC, null );
-	}
-
-	return function( $first, $second ) use ( &$criteria ) {
-		foreach ( $criteria as $criterion ) {
-			// How will we compare this round?
-			list( $column, $sortOrder, $projection ) = $criterion;
-			$sortOrder = $sortOrder === SORT_DESC ? -1 : 1;
-
-			// If a projection was defined project the values now
-			if ( $projection ) {
-				$lhs = call_user_func( $projection, $first[ $column ] );
-				$rhs = call_user_func( $projection, $second[ $column ] );
-			} else {
-				$lhs = $first[ $column ];
-				$rhs = $second[ $column ];
-			}
-			
-			// Do the actual comparison; do not return if equal
-			if ( $lhs < $rhs ) {
-				return -1 * $sortOrder;
-			}
-			else if ( $lhs > $rhs ) {
-				return 1 * $sortOrder;
-			}
-		}
-		
-		return 0; // tiebreakers exhausted, so $first == $second
-	};
-}
-usort( $addons, make_comparer( 'title' ) );
+// sort
+$arraytitle = array_column($addons, 'title');
+array_multisort($arraytitle, SORT_NATURAL|SORT_FLAG_CASE, $addons);
 
 $length = count( $addons );
 for ( $i = 0; $i < $length; $i++ ) {
