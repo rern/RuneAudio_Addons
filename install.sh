6b438c628724032f4a3ba5f158e7b5aa04ac745d
rern <rernrern@gmail.com>
2017-09-18 21:34:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,10 +50,11 @@ if ! grep -q 'id="addons"' $file; then
 fi
 
 file=/srv/http/app/templates/footer.php
-if ! grep -q 'addons.js' $file; then
-	echo $file
-	echo '<script src="<?=$this->asset('"'"'/js/addons.js'"'"')?>"></script>' >> $file
-fi
+echo $file
+! grep -q 'addons.js' $file &&
+echo '<script src="<?=$this->asset('"'"'/js/addons.js'"'"')?>"></script>' >> $file
+! grep -q 'hammer.min.js' $file && 
+echo '<script src="<?=$this->asset('"'"'/js/vendor/hammer.min.js'"'"')?>"></script>' >> $file
 
 # set sudo no password #######################################
 echo 'http ALL=NOPASSWD: ALL' > /etc/sudoers.d/http
