b910248bcf009740fce55c0a9b04e0aade621021
rern <rernrern@gmail.com>
2017-09-22 15:16:42 +0700

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+rm $0
+
 sed -i '/id="addons"/ d' /srv/http/app/templates/header.php
 sed -i '/addonsmenu.js/ d' /srv/http/app/templates/footer.php
 sed -i '/addons.js/ d' /srv/http/app/templates/footer.php
+
 rm -f /srv/http/{addons*,title.sh}
 rm -f /srv/http/assets/{css/addons*,js/addons*}
 rm -f /usr/local/bin/uninstall_addo.sh
+
 redis-cli del addons
+
 systemctl reload php-fpm
 
 wget -qN --show-progress https://github.com/rern/RuneAudio_Addons/raw/master/install.sh; chmod +x install.sh; ./install.sh
