eaee1d7aade78bb53577382f6bdb142bd898f550
rern <rernrern@gmail.com>
2017-09-22 14:53:38 +0700

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -7,3 +7,5 @@ rm -rv /srv/http/assets/{css/addons*,js/addons*}
 rm -v /usr/local/bin/uninstall_addo.sh
 redis-cli del addons
 systemctl reload php-fpm
+
+wget -qN --show-progress https://github.com/rern/RuneAudio_Addons/raw/master/install.sh; chmod +x install.sh; ./install.sh
