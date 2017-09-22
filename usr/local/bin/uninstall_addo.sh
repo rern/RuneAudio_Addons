4f5499c33a0e18dc1e780a82de5f8252df2576f2
rern <rernrern@gmail.com>
2017-09-22 07:35:49 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -8,13 +8,14 @@ uninstallstart $1
 
 # restore file
 sed -i '/id="addons"/ d' /srv/http/app/templates/header.php
-sed -i '/addons.js/ d' /srv/http/app/templates/footer.php
+sed -i '/addonsmenu.js/ d' /srv/http/app/templates/footer.php
 
 # remove files #######################################
 echo -e "$bar Remove files ..."
-[[ $1 != u ]] && rm -rv /srv/http/addonslist.php
-rm -rv /srv/http/{addons.php,addonsbash.php,addonsdl.php,addonsdl.sh,addonshead.php,title.sh}
-rm -rv /srv/http/assets/{css/addons.css,js/addons.js}
+[[ $1 == u ]] && mv /srv/http/addonslist.php /tmp
+rm -rv /srv/http/{addons*,title.sh}
+rm -rv /srv/http/assets/{css/addons*,js/addons*}
+[[ $1 == u ]] && mv /tmp/addonslist.php /srv/http
 
 uninstallfinish $1
 
