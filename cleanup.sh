96e96f1ff619142d55b93394d4ef5ab99ca54ab8
rern <rernrern@gmail.com>
2017-09-22 15:15:09 +0700

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -3,9 +3,9 @@
 sed -i '/id="addons"/ d' /srv/http/app/templates/header.php
 sed -i '/addonsmenu.js/ d' /srv/http/app/templates/footer.php
 sed -i '/addons.js/ d' /srv/http/app/templates/footer.php
-rm -rv /srv/http/{addons*,title.sh}
-rm -rv /srv/http/assets/{css/addons*,js/addons*}
-rm -v /usr/local/bin/uninstall_addo.sh
+rm -f /srv/http/{addons*,title.sh}
+rm -f /srv/http/assets/{css/addons*,js/addons*}
+rm -f /usr/local/bin/uninstall_addo.sh
 redis-cli del addons
 systemctl reload php-fpm
 
