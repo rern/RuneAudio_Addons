f051111767538ae4d468151c403fd498f8048f4e
rern <rernrern@gmail.com>
2017-09-24 10:45:18 +0700

    Update addonshead.php

--- srv/http/addonshead.php
+++ srv/http/addonshead.php
@@ -48,5 +48,3 @@ if (scrollbarWidth !== 0) {
 	<div id="loaderbg"></div>
 	<div id="loadercontent"><i class="fa fa-refresh fa-spin"></i>connecting...</div>
 </div>
-
-<?php require_once( 'addonsinfo.php' );?>
