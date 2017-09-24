fa7f3968059a8822b9d2dbfaff071c713d4d1d93
rern <rernrern@gmail.com>
2017-09-24 10:44:50 +0700

    Add files via upload

--- srv/http/assets/js/addonsinfo.js
+++ srv/http/assets/js/addonsinfo.js
@@ -1,3 +1,36 @@
+var infohtml = '\
+<div id="infoOverlay">\
+	<div id="infoBox">\
+		<div id="infoTopBg">\
+			<div id="infoTop">\
+				<a id="infoIcon"></a>&emsp;<a id="infoTitle"></a>\
+			</div>\
+			<div id="infoX"><i class="fa fa-times fa-lg"></i></div>\
+			<div style="clear: both"></div>\
+		</div>\
+		<div id="infoContent">\
+			<p id="infoMessage" class="info"></p>\
+			<div id="infoText" class="info">\
+				<a id="infoTextLabel"></a><input type="text" class="infoBox" id="infoTextbox">\
+			</div>\
+			<div id="infoPassword" class="info">\
+				<a id="infoPasswordLabel"></a> <input type="password" class="infoBox" id="infoPasswordbox">\
+			</div>\
+			<div id="infoRadio" class="info"></div>\
+			<div id="infoCheckbox" class="info"></div>\
+			<div id="infoSelect" class="info">\
+				<a id="infoSelectLabel"></a><select class="infoBox" id="infoSelectbox"></select>\
+			</div>\
+		</div>\
+		<div id="infoButtons">\
+			<a id="infoCancel" class="btn btn-default"></a>\
+			<a id="infoOk" class="btn btn-primary"></a>\
+		</div>\
+	</div>\
+</div>\
+'
+$( 'body' ).append( infohtml );
+
 function info( option ) {
 	// reset to default
 	$( '#infoIcon' ).html( '<i class="fa fa-question-circle fa-lg">' );
