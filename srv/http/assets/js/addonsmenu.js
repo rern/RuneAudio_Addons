e76a6bd286da4f41e4819edb4b745895aa6d57bb
rern <rernrern@gmail.com>
2017-09-30 15:57:52 +0700

    Update addonsmenu.js

--- srv/http/assets/js/addonsmenu.js
+++ srv/http/assets/js/addonsmenu.js
@@ -1,16 +1,13 @@
 $( '#addons' ).click( function() {
 	// fix path if click in other menu pages
-	var path = /\/.*\//.test( location.pathname ) ? '../../' : '';
-	var filesh = ( $( '#addo .btn' ).first().text() === ' Update' ) ? 'addonsupdate.sh' : 'addonsdl.sh';
-	
+	var path = /\/.*\//.test( window.location.pathname ) ? '../../' : '';
 	$( '#loader' ).removeClass( 'hide' );
-	
 	$.post( 
 		path +'addonssudo.php',
-		{ file: filesh },
+		{file: 'addonsdl.php'},
 		function( data ) {
 			if ( data == 1 ) {
-				location.href = path +'addons.php';
+				window.location.href = path +'addons.php';
 			} else {
 				alert( "Addons server cannot be reached.\n"
 					+"Please try again later." );
