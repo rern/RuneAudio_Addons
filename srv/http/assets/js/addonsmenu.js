cc0769b2af09906fb43b5923aa6b1eefd615a9d2
rern <rernrern@gmail.com>
2017-09-30 12:02:18 +0700

    Add files via upload

--- srv/http/assets/js/addonsmenu.js
+++ srv/http/assets/js/addonsmenu.js
@@ -2,13 +2,17 @@ $( '#addons' ).click( function() {
 	// fix path if click in other menu pages
 	var path = /\/.*\//.test( window.location.pathname ) ? '../../' : '';
 	$( '#loader' ).removeClass( 'hide' );
-	$.get( path +'addonsdl.php', function( data ) {
-		if ( data == 1 ) {
-			window.location.href = path +'addons.php';
-		} else {
-			alert( "Addons server cannot be reached.\n"
-				+"Please try again later." );
-			$( '#loader' ).addClass( 'hide' );
+	$.post( 
+		path +'addonssudo.php',
+		{file: 'addonsdl.php'},
+		function( data ) {
+			if ( data == 1 ) {
+				window.location.href = path +'addons.php';
+			} else {
+				alert( "Addons server cannot be reached.\n"
+					+"Please try again later." );
+				$( '#loader' ).addClass( 'hide' );
+			}
 		}
-	});
+	);
 });
