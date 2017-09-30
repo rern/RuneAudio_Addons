71a9875ae725ea5806e1768ab2ef456adb978c65
rern <rernrern@gmail.com>
2017-09-30 23:23:01 +0700

    Delete addonsupdate.sh

--- srv/http/addonsupdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/uninstall_addo.sh
-
-wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http
-chmod 755 /srv/http/install.sh
-/srv/http/install.sh
-
-[[ $? == 0 ]] && echo success
