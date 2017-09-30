95737f3befd4ab06ef196a05c3d9ebccfd21543b
rern <rernrern@gmail.com>
2017-09-30 14:26:33 +0700

    Update addonsupdate.sh

--- srv/http/addonsupdate.sh
+++ srv/http/addonsupdate.sh
@@ -2,6 +2,6 @@
 
 /usr/local/bin/uninstall_addo.sh u
 
-wget -qN https://github.com/rern/RuneAudio_Addons/raw/master/install.sh -P /srv/http
+wget -qN https://github.com/rern/RuneAudio_Addons/raw/update/install.sh -P /srv/http
 chmod 755 /srv/http/install.sh
 /srv/http/install.sh u
