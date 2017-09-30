a44ccd61a2ab3b63b7114046f288a2cbddb2af42
rern <rernrern@gmail.com>
2017-09-30 16:46:22 +0700

    Update addonssudo.php

--- srv/http/addonssudo.php
+++ srv/http/addonssudo.php
@@ -1,7 +1,8 @@
 <?php
 exec('/usr/bin/sudo /srv/http/'.$_POST[ 'file' ].';', $output, $exit);
 
-if ($exit === 0) {
-	opcache_reset();
-	echo 1;
-}
+if ($exit !== 0) die();
+
+echo $output;
+
+opcache_reset();
