286341887f565395538c88529ef15f644f009f85
rern <rernrern@gmail.com>
2017-09-30 17:51:56 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -7,16 +7,14 @@ alias=addo
 uninstallstart $1
 
 # restore file
-sed -i '/id="addons"/ d' /srv/http/app/templates/header.php
+sed -i '\|//addo0|, \|//addo1| d' /srv/http/app/templates/header.php
 sed -i '/addonsmenu.js/ d' /srv/http/app/templates/footer.php
 
 # remove files #######################################
 echo -e "$bar Remove files ..."
-[[ $1 == u ]] && mv /srv/http/addonslist.php /tmp
 rm -rv /srv/http/addons*
 rm -rv /srv/http/assets/css/addons*
 rm -rv /srv/http/assets/js/addons*
 [[ ! -e /usr/local/bin/uninstall_enha.sh ]] && rm -v srv/http/assets/js/vendor/hammer*
-[[ $1 == u ]] && mv /tmp/addonslist.php /srv/http
 
 uninstallfinish $1
