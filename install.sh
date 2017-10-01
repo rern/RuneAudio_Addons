c5e89b231b7213b476aa315cbe4b945a0530ac72
rern <rernrern@gmail.com>
2017-10-01 17:22:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ wgetnc https://github.com/rern/RuneAudio_Addons/archive/master.zip
 echo -e "$bar Install new files ..."
 rm -rf  /tmp/install
 mkdir -p /tmp/install
-bsdtar -xf master.zip --strip 1 -C /tmp/install
+bsdtar -xvf master.zip --strip 1 -C /tmp/install
 
 rm master.zip /tmp/install/* &> /dev/null
 chown -R http:http /tmp/install/srv
