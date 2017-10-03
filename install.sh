5a62f41474beb2a0764efc8d9518617d2a2ffd01
rern <rernrern@gmail.com>
2017-10-03 11:01:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,8 +29,8 @@ rm master.zip /tmp/install/* &> /dev/null
 chown -R http:http /tmp/install/srv
 chmod -R 755 /tmp/install
 
-cp -rp /tmp/install/* /
-rm -r /tmp/install
+mv -f /tmp/install/* /
+rm -rf /tmp/install
 
 # modify files #######################################
 echo -e "$bar Modify files ..."
