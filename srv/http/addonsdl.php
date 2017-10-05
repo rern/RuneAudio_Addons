aaaff63ea0b924ef877b2146ef62883a9832938d
rern <rernrern@gmail.com>
2017-10-05 20:27:47 +0700

    Update addonsdl.php

--- srv/http/addonsdl.php
+++ srv/http/addonsdl.php
@@ -3,8 +3,6 @@ $branch = ( isset( $_GET[ 'branch' ] ) ) ? $_GET[ 'branch' ] : '';
 
 exec( '/usr/bin/sudo /srv/http/addonsdl.sh '.$branch, $output, $exit );
 
-if ( $exit !== 0 ) die( 'failed' );
-
-echo 1;
+echo $exit;
 
 opcache_reset();
