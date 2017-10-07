ea1e1c051e9776e423bbab1830426f18f548f49a
rern <rernrern@gmail.com>
2017-10-07 20:37:51 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -1,7 +1,7 @@
 <?php
 $addonsversion = '1'; // temp: for update before 20171005 - to be removed
 
-$news = '<legend><p>News</p><br></legend><br>';
+//$news = '<legend><p>News</p><br></legend><br>';
 $revision = '
 	<li>Improve on-screen status while updating</li>
 ';
