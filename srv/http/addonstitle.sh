3649c7214a04bf22ecbd845b0a52d33ee92bc54e
rern <rernrern@gmail.com>
2017-10-09 15:31:26 +0700

    Update addonstitle.sh

--- srv/http/addonstitle.sh
+++ srv/http/addonstitle.sh
@@ -148,7 +148,7 @@ getuninstall() {
 	uninstallfile=${installurl/install.sh/uninstall_$alias.sh}
 	wgetnc $uninstallfile -P /usr/local/bin
 	if [[ $? != 0 ]]; then
-		title -l '=' "$warn Uninstall file download failed. $alias"
+		title -l '=' "$warn Uninstall file download failed."
 		title -nt "Please try install again."
 		exit
 	fi
