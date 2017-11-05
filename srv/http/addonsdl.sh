99d193fbef4b390e6d417b1f3cc1306f7cb82c72
rern <rernrern@gmail.com>
2017-11-05 21:26:59 +0700

    Update addonsdl.sh

--- srv/http/addonsdl.sh
+++ srv/http/addonsdl.sh
@@ -31,12 +31,11 @@ versionlist=$( echo "$addonslist" | grep 'version.*=>' | cut -d "'" -f 4 )
 versionredis=$( redis-cli hget addons addo )
 
 if [[ $versionlist != $versionredis ]]; then
-	freekb=$( df | grep '/$' | awk '{print $4}' )
-	if (( $freekb < 1000 )); then
-		[[ -n "$(ls -A /var/cache/pacman/pkg)" ]] && exit 2
+	if (( $( df | grep '/$' | awk '{print $4}' ) < 1000 )); then
+		# get directory size if enough after delete files
+		(( $( du /var/cache/pacman/pkg | awk '{print $1}' ) < 1000 )) && exit 2
 		rm /var/cache/pacman/pkg/*
-		freekb=$( df | grep '/$' | awk '{print $4}' )
-		(( $freekb < 1000 )) && exit 2
+		(( $( df | grep '/$' | awk '{print $4}' ) < 1000 )) && exit 2
 	fi
 	curl -s -v -X POST 'http://localhost/pub?id=addons' -d 1
 	wget -qN $installurl -P /srv/http
