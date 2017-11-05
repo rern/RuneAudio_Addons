9349e50d5384233ca58fa29641d146951575b2bf
rern <rernrern@gmail.com>
2017-11-05 18:55:52 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -4,11 +4,8 @@ $addons = array(
 array(
 	'alias'        => 'addo',
 	'title'        => 'Addons Menu',
-	'version'      => '20171025',
-	'revision'     => '<li>Add <white>update revision</white> of each addon</li>
-					<li>Add long-press Install for <white>branch testing</white></li>
-					<li>Remove long-press Uninstall for <white>forced update</white></li>
-					<li>Improve script templates</li>',
+	'version'      => '20171111',
+	'revision'     => '<li>Check available disk space before update</li>',
 	'maintainer'   => 'r e r n',
 	'description'  => 'This Addons Menu main page.',
 	'thumbnail'    => 'https://github.com/rern/_assets/raw/master/RuneAudio_Addons/addonsthumb.png',
