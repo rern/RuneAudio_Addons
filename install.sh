b1d2a9fe10526cbe135d79053801b78902d65017
rern <rernrern@gmail.com>
2017-11-10 06:50:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 # $1-branch ; $2-branch flag '-b' (syntax for all addons in addonsdl.sh)
 
-hwrevision=$( cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' )
-hwrev=${hwrevision:0:2}
-if [[ $hwrev == 00 || $hwrev == 90 ]];then
+hwrevision=$( cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | cut -b 1-2 )
+if [[ $hwrevision == 00 || $hwrevision == 90 ]];then
 	echo 'Addons Menu cannot be used with this RPi hardware.'
 	exit
 fi
