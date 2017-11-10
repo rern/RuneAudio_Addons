53429c6da6c19f708068e9875b11632ca3691f10
rern <rernrern@gmail.com>
2017-11-10 19:25:09 +0700

    Add files via upload

--- srv/http/addons.php
+++ srv/http/addons.php
@@ -1,12 +1,14 @@
 <?php
 require_once( 'addonshead.php' );
+
+$rpiversion = fgets( fopen( '/proc/device-tree/model', 'r' ) );
+$diskspace = number_format( round( disk_free_space( '/' ) / 1024 / 1024 ) );
 // -------------------------------------------------------------------------------------------------
-$indexaddo = array_search( 'addo', array_column( $addons, 'alias' ) );
-$addonsversion = $addons[ $indexaddo ][ 'version' ];
 echo '
 	<div class="container">
 	<h1>ADDONS</h1><a id="close" href="/"><i class="fa fa-times fa-2x"></i></a>
-	<a class="issues" href="http://www.runeaudio.com/forum/addons-menu-install-addons-the-easy-way-t5370-1000.html" target="_blank">
+	<legend class="bl">'.$rpiversion.'<p style="float:right;"> available space: '.$diskspace.' MB</p></legend>
+	<a id="issues" href="http://www.runeaudio.com/forum/addons-menu-install-addons-the-easy-way-t5370-1000.html" target="_blank">
 			issues&ensp;<i class="fa fa-external-link"></i>
 	</a>
 ';
@@ -18,32 +20,33 @@ $GLOBALS[ 'release' ] = $redis->get( 'release' );
 $GLOBALS[ 'redis' ] = $redis->hGetAll( 'addons' );
 $GLOBALS[ 'list' ] = '';
 $GLOBALS[ 'blocks' ] = '';
+//$GLOBALS[ 'addons' ] = $addons;
 
 // sort
 $arraytitle = array_column( $addons, 'title' );
 $arraytitle[ 0 ] = 0;
 array_multisort( $arraytitle, SORT_NATURAL | SORT_FLAG_CASE, $addons );
 $arraytitle[ 0 ] = 'Addons Menu';
+$arrayalias = array_keys( $addons );
 
-$length = count( $addons );
-for ( $i = 0; $i < $length; $i++ ) {
-	addonblock( $addons[ $i ] );
+foreach( $arrayalias as $alias ) {
+	addonblock( $alias );
 }
 // -------------------------------------------------------------------------------------------------
 echo '
 	<ul id="list">'.
-	$list.'
+		$list.'
 	</ul>
 	<br>
 ';
 echo $blocks;
 // -------------------------------------------------------------------------------------------------
-function addonblock( $addon ) {
+function addonblock( $alias ) {
+	$addon = $GLOBALS[ 'addons' ][ $alias ];
 	if ( $GLOBALS[ 'release' ] == '0.4b' && isset( $addon[ 'only03' ] ) ) return;
 	
 	$thumbnail = isset( $addon[ 'thumbnail' ] ) ? $addon[ 'thumbnail' ] : '';
 	$buttonlabel = isset( $addon[ 'buttonlabel' ]) ? $addon[ 'buttonlabel' ] : 'Install';
-	$alias = $addon[ 'alias' ];
 	
 	if ( $GLOBALS[ 'redis' ][ $alias ] || file_exists( "/usr/local/bin/uninstall_$alias.sh" ) ) {
 		$check = '<i class="fa fa-check"></i> ';
@@ -51,19 +54,13 @@ function addonblock( $addon ) {
 			// !!! mobile browsers: <button>s submit 'formtemp' with 'get' > 'failed', use <a> instead
 			$btnin = '<a class="btn btn-default disabled"><i class="fa fa-check"></i> '.$buttonlabel.'</a>';
 		} else {
+			$check = '<i class="fa fa-refresh"></i> ';
 			$btnin = '<a class="btn btn-primary"><i class="fa fa-refresh"></i> Update</a>';
 		}
 		$btnun = '<a class="btn btn-default"><i class="fa fa-close"></i> Uninstall</a>';
 	} else {
-		if ( isset( $addon[ 'option' ])) {
-			$addonoption = preg_replace( '/\n|\t/', '', $addon[ 'option' ] );
-			$addonoption = htmlspecialchars( $addonoption );
-			$option = 'option="'.$addonoption.'"';
-		} else {
-			$option = '';
-		}
 		$check = '';
-		$btnin = '<a class="btn btn-default btnin" '.$option.'><i class="fa fa-check"></i> '.$buttonlabel.'</a>';
+		$btnin = '<a class="btn btn-default btnin"><i class="fa fa-check"></i> '.$buttonlabel.'</a>';
 		$btnun = '<a class="btn btn-default disabled"><i class="fa fa-close"></i> Uninstall</a>';
 	}
 	
@@ -77,8 +74,13 @@ function addonblock( $addon ) {
 	// addon blocks -------------------------------------------------------------
 	$version = isset( $addon[ 'version' ] ) ? $addon[ 'version' ] : '';
 	$revisionclass = $version ? 'revision' : 'revisionnone';
-	$detail = ' <a href="'.$addon[ 'sourcecode' ].'" target="_blank">&emsp;detail &nbsp;<i class="fa fa-external-link"></i></a>';
-	if ( !$addon[ 'sourcecode' ] ) $detail = '';
+	$revision = '<li>'.str_replace( '<br>', '</li><li>', $addon[ 'revision' ] ).'</li>';
+	$sourcecode = $addon[ 'sourcecode' ];
+	if ( $sourcecode ) {
+		$detail = ' <a href="'.$sourcecode.'" target="_blank">&emsp;detail &nbsp;<i class="fa fa-external-link"></i></a>';
+	} else {
+		$detail = '';
+	}
 	$GLOBALS[ 'blocks' ] .= '
 		<div id="'.$alias.'" class="boxed-group">';
 	if ( $thumbnail ) $GLOBALS[ 'blocks' ] .= '
@@ -89,11 +91,9 @@ function addonblock( $addon ) {
 				&emsp;<p><a class="'.$revisionclass.'">'.$version.'</a>
 				&ensp;by<white>&ensp;'.$addon[ 'maintainer' ].'</white></p>
 			</legend>
-			<div class="detail" style="display: none;">
-				<ul>'
-					.$addon[ 'revision' ].'
-				</ul>
-			</div>
+			<ul style="display: none;">'
+				.$revision.'
+			</ul>
 			<form class="form-horizontal" alias="'.$alias.'">
 				<p>'.$addon[ 'description' ].$detail.'</p>'
 				.$btnin; if ( $version ) $GLOBALS[ 'blocks' ] .= ' &nbsp; '.$btnun;
@@ -111,6 +111,9 @@ function addonblock( $addon ) {
 </div>
 <div id="bottom"></div>
 
+<script>
+addons = JSON.parse( '<?php echo json_encode( $GLOBALS[ 'addons' ] );?>' );
+</script>
 <script src="assets/js/vendor/jquery-2.1.0.min.js"></script>
 <script src="assets/js/vendor/hammer.min.js"></script>
 <script src="assets/js/addonsinfo.js"></script>
