bc73eaf26e2a16ea1f5ae90bba2da77736bdb2a3
rern <rernrern@gmail.com>
2017-11-28 09:19:55 +0700

    Add files via upload

--- srv/http/restoreui.php
+++ srv/http/restoreui.php
@@ -8,6 +8,32 @@ exec( '/usr/bin/sudo /usr/bin/redis-cli del addons' );
 // clear cache must be before echo
 opcache_reset();
 
-echo '<br>
-RuneUI restored to default.<br>
-Reinstall Addons Menu: Menu > Addons';
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/runeui.css">
+    <link rel="stylesheet" href="assets/css/addonsinfo.css">
+</head>
+<body>
+	
+<script src="assets/js/vendor/jquery-2.1.0.min.js"></script>
+<script src="assets/js/addonsinfo.js"></script>
+<script>
+$(function() {
+	info( {
+		  title    : "RuneUI Restore"
+		, message  : "RuneUI restored to default."
+		, cancel   : function() {
+			location.href = "/";
+		}
+		, ok       : function() {
+			location.href = "/";
+		}
+	} );
+} );
+</script>
+
+</body>
+</html>';
