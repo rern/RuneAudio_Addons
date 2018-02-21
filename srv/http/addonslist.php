cd13e916246ef3342b55a647ec6974dc3b9ecc7d
rern <rernrern@gmail.com>
2018-02-21 19:35:11 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -44,8 +44,8 @@ $addons = array(
 'addo' => array(
 	'title'        => 'Addons Menu',
 	'version'      => '20180222',
-	'revision'     => 'Enable <white>abort install</white>, but should not do it, by <i class=\"fa fa-times\"></i>'
-					.' or browser <i class=\"fa fa-times-circle\"></i> <i class=\"fa fa-arrow-circle-left\"></i> buttons'
+	'revision'     => 'Enable <white>abort install</white> but should not do it unless necessary. ( <i class=\"fa fa-times\"></i>'
+					.' or browser <i class=\"fa fa-times-circle\"></i> <i class=\"fa fa-arrow-circle-left\"></i> buttons)'
 					.'<br>...'
 					.'<br>Support <white>uninstall conflict addons</white>'
 					.'<br>Support <white>hide redundant features addons</white>'
