c041949cf72e58793221f13e2db34e1e88f238a3
rern <rernrern@gmail.com>
2018-03-07 17:02:31 +0700

    Update addons.php

--- srv/http/addons.php
+++ srv/http/addons.php
@@ -9,7 +9,7 @@ $redisaddons = $redis->hGetAll( 'addons' );
 // -------------------------------------------------------------------------------------------------
 echo '
 <div class="container">
-	<a id="close" href="/"><i class="fa fa-times fa-2x"></i></a>
+	<a id="close" class="close-root" href="/"><i class="fa fa-times fa-2x"></i></a>
 	<h1>ADDONS</h1>
 	<legend class="bl">RuneAudio '.$runeversion.' ● available: '.number_format( $available ).' MB'.$expandable.'</legend>
 	<a id="issues" href="http://www.runeaudio.com/forum/addons-menu-install-addons-the-easy-way-t5370-1000.html" target="_blank">
