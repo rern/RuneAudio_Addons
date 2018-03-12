07e297933573e7dc93bf38f6b7b50ca77a36dc2d
rern <rernrern@gmail.com>
2018-03-12 16:27:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,8 +31,6 @@ installstart $@
 
 getinstallzip
 
-wgetnc https://github.com/rern/RuneUI_enhancement/raw/master/srv/http/enhanceredis.php -P /srv/http
-
 if [[ $( redis-cli get release ) == 0.4b ]]; then
     rm -r /srv/http/assets/default
 	mv /srv/http/assets/default{04,}
