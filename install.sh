9ac0d2f4781549831784abd4e861cf24dfc03600
rern <rernrern@gmail.com>
2018-03-12 21:15:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -84,6 +84,7 @@ WantedBy=multi-user.target
 crontab -l | { cat; echo '00 01 * * * /srv/http/addonsupdate.sh &
 00 13 * * * /srv/http/addonsupdate.sh &'; } | crontab -
 systemctl enable addons cronie
+systemctl daemon-reload
 systemctl start addons cronie
 
 redis-cli hset addons update 0 &>/dev/null
