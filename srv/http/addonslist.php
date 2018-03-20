df85a4fd3014bdc0e170e1dfbc72d16133e59e68
rern <rernrern@gmail.com>
2018-03-20 14:01:44 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -239,7 +239,7 @@ $addons = array(
 'enha' => array(
 	'title'        => 'RuneUI Enhancements *',
 	'version'      => '20180320',
-	'revision'     => 'Major improvement - <white>knobs and buttons</white>'
+	'revision'     => 'Major improvement - <white>knobs and buttons</white> (switched from knob.js to roundSlider.js)'
 					.'<br>Enable <white>mute button light</white> on all screens'
 					.'<br>Volume: number = mute, over number = volume+, under number = volume-'
 					.'<br>...'
