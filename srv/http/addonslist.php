92ba7088f6be2f4f14d3e5b34e23250703c14f32
rern <rernrern@gmail.com>
2018-03-27 09:29:35 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -244,7 +244,7 @@ $addons = array(
 	'revision'     => 'Fix long-press volume up/down'
 					.'<br>Improve time knob refresh position'
 					.'<br>Time knob shows time change while drag'
-					.'<br>Lower time knob refresh rate on local browser to keep low CPU clycle'
+					.'<br>Lower time knob refresh rate on local browser to lower CPU load'
 					.'<br>Switch from countdown.js to normal setInterval()'
 					.'<br>...'
 					.'<br>Major improvement - <white>knobs and buttons</white> (switched from knob.js to roundSlider.js)'
