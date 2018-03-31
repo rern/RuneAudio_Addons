092e45d27abaded4d18de81cd79602500ef8f078
rern <rernrern@gmail.com>
2018-03-31 13:18:20 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -243,6 +243,7 @@ $addons = array(
 	'version'      => '20180324',
 	'revision'     => 'Now show while stop: sampling info and duration (except DSD)'
 					.'<br>Now work while stop: previous, next, tab/drag time knob'
+					.'<br>Now hide: leading 00:0 time and hide time while stop or streaming'
 					.'<br>Time change along with time knob drag'
 					.'<br>Lower time knob refresh rate on local browser to lower CPU load'
 					.'<br>Switch from countdown.js to normal setInterval()'
