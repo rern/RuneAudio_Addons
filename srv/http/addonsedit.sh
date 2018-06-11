9d95bc03c6abe3a23378fcd93e8a1497a1e545f6
rern <rernrern@gmail.com>
2018-06-11 21:39:58 +0700

    Update addonsedit.sh

--- srv/http/addonsedit.sh
+++ srv/http/addonsedit.sh
@@ -2,30 +2,28 @@
 
 # usage:
 #     pre-defined variables:
-#             alias=name                  # already in install.sh / uninstall_alias.sh
+#         alias=name                      # already in install.sh / uninstall_alias.sh
+#
 #         file=/path/file                 # before all commands of each file
 #         string=( cat <<'EOF'            # before each insert and append
-#         DO escape \ backslash\n\
-#         DO end every lines with \\n\\ \n\
-#         DON'T put spaces or any characters in closing heredoc\n\
-#         DON'T end last line with \ backslash
+#         DON'T put spaces or any characters in closing EOF
+#         only EOF tag at 1st of the ending line with nothing else
 #         EOF
 #         )
 #    command:
-#         insert 'regex'                  # //alias lines onclose $string
-#         insertphp 'regex'               # <?php /* and */ ?> lines onclose $string
-#         append 'regex'                  # same as insert
-#         appendphp 'regex'               # same as insertphp
-#             comment after insert/append for the same line (avoid $string with 'regex')
 #         comment 'regex' ['regex2']      # /* existing */
-#         commentphp 'regex' ['regex2']   # <?php /* existing */ ?>
+#         commentP 'regex' ['regex2']     # <?php /* existing */ ?>
 #             /* other comment */ CAN'T be inside 'regex' 'regex2'
+#         insert 'regex'                  # //alias and //alias enclose $string
+#         insertP 'regex'                 # <?php /* and */ ?> lines enclose $string
+#         append 'regex'                  # same as insert
+#         appendP 'regex'                 # same as insertphp
+#             insert/append with 'regex' in $string must be after comment to avoid double
 #
 #         restorefile file [file2 ...]    # remove all insert / append / comment
 #    'regex':
 #         search pattern must be single quoted and escaped properly
 #         default delimiter = |
-
 
 comment() {
 	if [[ $1 == -p ]]; then
@@ -36,6 +34,7 @@ comment() {
 		front='/*'$alias
 		back=$alias'*/'
 	fi
+	
 	if (( $# == 1 )); then
 		sed -i -e "\|$1| { s|^|$front|; s|$|$back| }" "$file"
 	else
@@ -43,38 +42,41 @@ comment() {
 	fi
 }
 
-add() {
+insert() {
 	if [[ $1 == -p ]]; then
-		upper='<?php //'$alias'0 ?>\n\'
-		lower='\n<?php //'$alias'1 ?>'
+		upper='<?php //'$alias'0 ?>\n'
+		lower='n<?php //'$alias'1 ?>'
 		shift
 	else
-		upper='//'$alias'0\n\'
-		lower='\n//'$alias'1'
+		upper='//'$alias'0\n'
+		lower='n//'$alias'1'
 	fi
 	
-	if [[ $1 == -i ]]; then
+	# escape \ and close eol with \ before passing to sed
+	string=$( cat <<EOF
+$( echo "$string" | sed 's/\\/\\\\/g; s/$/\\/' )
+EOF
+)
+	
+	if [[ $1 == -a ]]; then
 		shift
-		sed -i "\|$1| i$upper$string$lower" "$file"
-	else
 		sed -i "\|$1| a$upper$string$lower" "$file"
+	else
+		sed -i "\|$1| i$upper$string$lower" "$file"
 	fi
 }
 
-commentphp() {
+commentP() {
 	comment -p "$@"
 }
-insert() {
-	add -i "$1"
-}
-insertphp() {
-	add -p -i "$1"
+insertP() {
+	insert -p "$1"
 }
 append() {
-	add "$1"
+	insert -a "$1"
 }
-appendphp() {
-	add -p "$1"
+appendP() {
+	insert -p -a "$1"
 }
 
 restorefile() {
