bbb959e9a67641d3ea2b29e3f04b3405fe75d905
rern <rernrern@gmail.com>
2018-06-13 16:41:22 +0700

    Add files via upload

--- srv/http/addonsedit.sh
+++ srv/http/addonsedit.sh
@@ -1,97 +1,132 @@
 #!/bin/bash
 
-# usage:   comment 'pattern' ['pattern2'] file
-#          commentphp (use '<?php' comment)
+# insert / append / comment : for /*...*/  <?php /*...*/ ?>  #...
 #
-#          insert 'pattern' "$string" file
-#          insertphp (use '<?php' comment)
-#          append 'pattern' "$string" file
-#          appendphp (use '<?php' comment)
-#
-#          restorefile file
-#
-# pattern  sed search regex pattern (escaped + inside single quoted)
-# pattern2 bottom line of range (comment only)
-# $string  string to add: insert - above, append - below
-
-# string - singleline: DO escape \ and "
-
-# string - heredoc: all characters and symbols can be used
-#
-# string=$( cat <<'EOF'
-# aaa\n\
-# bbb
-# EOF
-# )
-#
-#    DO end every lines with \n\
-#    DO escape \ backslash
-#    DON'T put spaces or any characters in closing heredoc tag
-#    DON'T end last line with \ backslash
-
-example=$( cat <<'EOF'
-@#$&*()'"%-+=/;:!?€£¥_^[]{}§|~\\<>\n\
-DO end every lines with \\n\\\n\
-DO escape \\ backslash\n\
-DON'T put spaces or any characters in closing heredoc tag\n\
-DON'T end last line with \ backslash
-EOF
-)
+# pre-defined variables:
+#     alias=name                      # already in install.sh / uninstall_alias.sh
+#     file=/path/file                 # before all commands of each file
+#     string=( cat <<'EOF'            # before each insert and append
+#     place code inside this heredoc literally
+#     last line
+#     EOF
+#     )
+# usage:
+#     comment SEARCH [SEARCH2]        # /*alias line(s) alias*/
+#     commentP SEARCH [SEARCH2]       # <?php /*alias line(s) alias*/ ?>
+#     commentS SEARCH [SEARCH2]       # #alias (each line)
+#     insert SEARCH                   # //alias0
+#                                     # string
+#                                     # //alias1
+#     insertP SEARCH                  # <?php //alias0 ?>
+#                                     # string
+#                                     # <?php //alias1 ?>
+#     insertS SEARCH                  # #0alias
+#                                     # string
+#                                     # #1alias
+#     append SEARCH                   # same as insert
+#     appendP SEARCH                  # same as insertP
+#     appendS SEARCH                  # same as insertS
+#     restorefile FILE [FILE2 ...]    # remove all insert / append / comment
+# options:
+#     SEARCH pattern must be quoted and escaped
+#          "  $  `  \  inside "..."  use  \"  \$  \`  \\  or use  .  as wildcard
+#          '  inside '...'           use  "'"             or use  .  as wildcard
+#     insert/append with SEARCH itself in $string
+#          must be after comment to the same SEARCH (avoid commented after insert)
+#          must be combined with insert/append to the same SEARCH (avoid double insert)
 
 comment() {
 	if [[ $1 == -p ]]; then
-		upper='<?php if(0){//'$alias' ?>'
-		lower='<?php }//'$alias' ?>'
+		front='<?php /*'$alias
+		back=$alias'*/ ?>'
+		shift
+	elif [[ $1 == -s ]]; then
+		front='#'$alias
+		back=
 		shift
 	else
-		upper='if(0){//'$alias
-		lower='}//'$alias
+		front='/*'$alias
+		back=$alias'*/'
 	fi
-	if (( $# == 2 )); then
-		sed -i -e "/$1/ i$upper" -e "/$1/ a$lower" "$2"
+	# escape regex: reserved characters
+	regex=$( echo "$1" | sed -e 's|[]"\|$*.^[]|\\&|g' )
+	if (( $# == 1 )); then
+		if [[ $front != '#'$alias ]]; then
+			sed -i "\|$regex| { s|\*/|\*$alias/|; s|^|$front|; s|$|$back| }" "$file"
+		else
+			sed -i "\|$regex| s|^|$front|" "$file"
+		fi
 	else
-		sed -i -e "/$1/ i$upper" -e "/$2/ a$lower" "$3"
+		regex2=$( echo "$2" | sed -e 's|[]"\|$*.^[]|\\&|g' )
+		if [[ $front != '#'$alias ]]; then
+			# escape existing /* comment */
+			sed -i "\|$regex|, \|$regex2| s|\*/|\*$alias/|" "$file"
+			sed -i -e "\|$regex| s|^|$front|" -e "\|$regex2| s|$|$back|" "$file"
+		else
+			sed -i "\|$regex|, \|$regex2| s|^|$front|" "$file"
+		fi
 	fi
 }
 
-add() {
+insert() {
 	if [[ $1 == -p ]]; then
-		upper='<?php if(0){//'$alias' ?>\n\'
-		lower='\n<?php }//'$alias' ?>'
+		upper='<?php //0'$alias' ?>\n'
+		lower='n<?php //1'$alias' ?>'
+		shift
+	elif [[ $1 == -s ]]; then
+		upper='#0'$alias'\n'
+		lower='n#1'$alias
 		shift
 	else
-		upper='if(0){//'$alias'\n\'
-		lower='\n}//'$alias
+		upper='/*0'$alias'*/\n'
+		lower='n/*1'$alias'*/'
 	fi
 	
-	if [[ $1 == -i ]]; then
-		shift
-		sed -i "/$1/ i$upper$2$lower" "$3"
+	ia=i
+	[[ $1 == -a ]] && ia=a && shift
+	# escape \ and close each line with \ 
+	string=$( cat <<EOF
+$( echo "$string" | sed 's|\\|\\\\|g; s|$|\\|' )
+EOF
+)
+	# if line number or $ last line
+	if [[ $1 =~ [0-9]+$ || $1 == '$' ]]; then
+		sed -i "$1 $ia$upper$string$lower" "$file"
 	else
-		sed -i "/$1/ a$upper$2$lower" "$3"
+		regex=$( echo "$1" | sed -e 's|[]"\|$*.^[]|\\&|g' )
+		sed -i "\|$regex| $ia$upper$string$lower" "$file"
 	fi
 }
 
-commentphp() {
+commentP() {
 	comment -p "$@"
 }
-insert() {
-	add -i "$@"
+commentS() {
+	comment -s "$@"
 }
-insertphp() {
-	add -p -i "$@"
+insertP() {
+	insert -p "$1"
+}
+insertS() {
+	insert -s "$1"
 }
 append() {
-	add "$@"
+	insert -a "$1"
 }
-appendphp() {
-	add -p "$@"
+appendP() {
+	insert -p -a "$1"
+}
+appendS() {
+	insert -s -a "$1"
 }
 
 restorefile() {
-	sed -i -e "/${alias}0 ?>\s*$/, /${alias}1 ?>\s*$/ d
-	" -e "/${alias} ?>\s*$/ d
-	" -e "/${alias}0\s*$/, /${alias}1\s*$/ d
-	" -e "/${alias}\s*$/ d
-	" $1
+	for file in "$@"; do
+		sed -i -e "\|^<?php //0$alias ?>$|, \|^<?php //1$alias ?>$| d
+		" -e "/#0$alias/, /#1$alias/ d
+		" -e "\|^/\*0$alias\*/$|, \|^/\*1$alias\*/$| d
+		" -e "s/^<?php \/\*$alias\|$alias\*\/ ?>$\|^#$alias\/^\/\*$alias\|$alias\*\/$//g
+		" -e "s|\*${alias}/|\*/|
+		" "$file"
+	done
 }
