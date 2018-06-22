c98e2eef210df7e7cd31dadad76aea817566ae73
rern <rernrern@gmail.com>
2018-06-22 15:07:40 +0700

    Add files via upload

--- srv/http/addonsdl.php
+++ srv/http/addonsdl.php
@@ -1,4 +1,13 @@
 <?php
+if ( isset( $_POST[ 'redis' ] ) ) {
+	$redis = new Redis(); 
+	$redis->pconnect( '127.0.0.1' );
+
+	$update = $redis->hGet( 'addons', $_POST[ 'redis' ] );
+	echo $update;
+	die();
+}
+
 $branch = ( isset( $_GET[ 'branch' ] ) ) ? $_GET[ 'branch' ] : '';
 
 exec( '/usr/bin/sudo /srv/http/addonsdl.sh '.$branch, $output, $exit );
