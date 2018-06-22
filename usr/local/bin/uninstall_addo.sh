82cc3123df69f2da01a3793a0bce960741aefbca
rern <rernrern@gmail.com>
2018-06-22 20:27:25 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -7,19 +7,21 @@ alias=addo
 uninstallstart $@
 
 # restore file
+echo -e "$bar Restore and remove files ..."
 if [[ ! -e /usr/local/bin/uninstall_enha.sh ]]; then
 	restorefile /srv/http/app/templates/header.php /srv/http/app/templates/footer.php
+	rm -v /srv/http/assets/css/addons*
+	rm -v /srv/http/assets/js/addons*
 	rm -v /srv/http/assets/js/vendor/{hammer.min.js,propagating.js}
 else
 	sed -i '/id="addons"/d' /srv/http/app/templates/header.php
 	sed -i '/addonsmenu.js/ d' /srv/http/app/templates/footer.php
+	rm -v /srv/http/assets/css/addons.css
+	rm -v /srv/http/assets/js/{addons.js,addonsmenu.js}
 fi
 
 # remove files #######################################
-echo -e "$bar Remove files ..."
 rm -v /srv/http/addons*
-rm -v /srv/http/assets/css/addons*
-rm -v /srv/http/assets/js/addons*
 rm -rv /srv/http/assets/addons
 
 crontab -l | { cat | sed '/addonsupdate.sh/ d'; } | crontab -
