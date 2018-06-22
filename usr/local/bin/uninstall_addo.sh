110ecabeaa7c8f898237f988a1a6eeb4297f5926
rern <rernrern@gmail.com>
2018-06-22 22:12:54 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -12,8 +12,8 @@ echo -e "$bar Restore and remove files ..."
 if [[ ! -e /usr/local/bin/uninstall_enha.sh ]]; then
 	restorefile /srv/http/app/templates/header.php /srv/http/app/templates/footer.php
 	rm -v /srv/http/assets/css/addons*
-	rm -v /srv/http/assets/js/addons*
 	rm -v /srv/http/assets/js/vendor/{hammer.min.js,propagating.js}
+	rm /srv/http/assets/js/addons*
 else
 	sed -i '/id="addons"/d' /srv/http/app/templates/header.php
 	sed -i '/addonsmenu.js/ d' /srv/http/app/templates/footer.php
