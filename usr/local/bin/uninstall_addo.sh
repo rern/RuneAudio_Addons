83fab552928568694dd668cb03147a315027fb49
rern <rernrern@gmail.com>
2018-06-22 20:10:06 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -7,7 +7,13 @@ alias=addo
 uninstallstart $@
 
 # restore file
-restorefile /srv/http/app/templates/header.php /srv/http/app/templates/footer.php
+if [[ -e /usr/local/bin/uninstall_enha.sh ]]; then
+	sed -i '/id="addons"/d' /srv/http/app/templates/header.php
+	sed -i '/addonsmenu.js/ d' /srv/http/app/templates/footer.php
+else
+	restorefile /srv/http/app/templates/header.php /srv/http/app/templates/footer.php
+	rm -v /srv/http/assets/js/vendor/{hammer.min.js,propagating.js}
+fi
 
 # remove files #######################################
 echo -e "$bar Remove files ..."
@@ -15,19 +21,6 @@ rm -v /srv/http/addons*
 rm -v /srv/http/assets/css/addons*
 rm -v /srv/http/assets/js/addons*
 rm -rv /srv/http/assets/addons
-
-if [[ -e /usr/local/bin/uninstall_enha.sh ]]; then
-	file=/srv/http/app/templates/footer.php
-	string=$( cat <<'EOF'
-<script src="<?=$this->asset('/js/vendor/hammer.min.js')?>"></script>
-<script src="<?=$this->asset('/js/vendor/propagating.js')?>"></script>
-<script src="<?=$this->asset('/js/addonsinfo.js')?>"></script>
-EOF
-)
-	appendH '$'
-else
-	rm -v /srv/http/assets/js/vendor/{hammer.min.js,propagating.js}
-fi
 
 crontab -l | { cat | sed '/addonsupdate.sh/ d'; } | crontab -
 
