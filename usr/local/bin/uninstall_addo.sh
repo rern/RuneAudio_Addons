05bc5066a64850ce4920c97c1a0758851f85b09d
rern <rernrern@gmail.com>
2018-06-23 10:39:29 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -23,6 +23,25 @@ else
 	sed -i '/addonsmenu.js/ d' /srv/http/app/templates/footer.php
 	rm -v /srv/http/assets/css/addons.css
 	rm -v /srv/http/assets/js/{addons.js,addonsmenu.js}
+	
+	# 0temp0 fix
+	if ! grep 'addonsinfo.css' /srv/http/app/templates/header.php; then
+		string=$( cat <<'EOF'
+    <link rel="stylesheet" href="<?=$this->asset('/css/addonsinfo.css')?>">
+EOF
+)
+		sed -i "/runeui.css/ a$string" $file  # no enclosure tags for sharing with RuneUIe
+	fi
+	if ! grep 'hammer.min.js' /srv/http/app/templates/footer.php; then
+		string=$( cat <<'EOF'
+<script src="<?=$this->asset('/js/vendor/hammer.min.js')?>"></script>
+<script src="<?=$this->asset('/js/vendor/propagating.js')?>"></script>
+<script src="<?=$this->asset('/js/addonsinfo.js')?>"></script>
+EOF
+)
+		sed -i "$ a$string" $file  # no enclosure tags for sharing with RuneUIe
+	fi
+	# 1temp1 fix
 fi
 
 # remove files #######################################
