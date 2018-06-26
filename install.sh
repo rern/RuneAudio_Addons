d4ce4995e48f5f5067d63edd56adc6c2c436d1f5
rern <rernrern@gmail.com>
2018-06-26 12:13:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ fi
 # temp
 sed -i '/addonsinfo.css\|class="fa fa-addons"\|class="fa fa-cubes"/d' /srv/http/app/templates/header.php
 sed -i '/hammer.min.js\|propagating.js\|addonsinfo.js\|addonsmenu.js/ d'  /srv/http/app/templates/footer.php
+redis-cli hdel addons expa
 
 # for 'installstart' before 'addonslist.php' exist
 if [[ ! -e /srv/http/addonslist.php ]]; then
