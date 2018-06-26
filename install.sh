39bb3aaa98cc1e2151307235232d1eacae9bdb9d
rern <rernrern@gmail.com>
2018-06-26 12:51:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ ExecStart=/srv/http/addonsupdate.sh &
 WantedBy=multi-user.target
 ' > $file
 
-crontab -l | { cat; echo '00 01 * * * /srv/http/addonsupdate.sh &'; } | crontab -
+crontab -l | { cat; echo '00 01 * * * /srv/http/addonsupdate.sh &'; } | crontab - &> /dev/null
 systemctl enable addons cronie
 systemctl daemon-reload
 systemctl start addons cronie
