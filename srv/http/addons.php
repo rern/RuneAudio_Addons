44277446f592e5873c4a82b97785fd76f542c19f
rern <rernrern@gmail.com>
2018-07-02 09:33:15 +0700

    Add files via upload

--- srv/http/addons.php
+++ srv/http/addons.php
@@ -53,21 +53,21 @@ foreach( $arrayalias as $alias ) {
 	if ( isset( $addon[ 'buttonlabel' ] ) ) {
 		$buttonlabel = $addon[ 'buttonlabel' ];
 	} else {
-		$buttonlabel = '<i class="fa fa-plus-circle"></i> Install';
+		$buttonlabel = '<i class="fa fa-plus-circle"></i>&ensp;Install';
 	}
 	
 	if ( $redisaddons[ $alias ] || $redis->hGet( 'addons', $alias ) ) {
-		$check = '<i class="fa fa-check"></i> ';
+		$check = '<i class="fa fa-check status"></i> ';
 		if ( !isset( $addon[ 'version' ] ) 
 			|| $addon[ 'version' ] == $redisaddons[ $alias ] ) {
 			// !!! mobile browsers: <button>s submit 'formtemp' with 'get' > 'failed', use <a> instead
-			$btnin = '<a class="btn btn-default disabled">'.$buttonlabel.'</a>';
+			$btnin = '<a class="btn btn-default disabled">&ensp;'.$buttonlabel.'</a>';
 		} else {
-			$check = '<i class="fa fa-refresh"></i> ';
-			$btnin = '<a class="btn btn-primary"><i class="fa fa-refresh"></i> Update</a>';
+			$check = '<i class="fa fa-refresh status"></i> ';
+			$btnin = '<a class="btn btn-primary"><i class="fa fa-refresh"></i>&ensp;Update</a>';
 		}
 		$btnunattr = isset( $addon[ 'rollback' ] ) ?' rollback="'.$addon[ 'rollback' ].'"' : '';
-		$btnun = '<a class="btn btn-default btnbranch"'.$btnunattr.'><i class="fa fa-minus-circle"></i> Uninstall</a>';
+		$btnun = '<a class="btn btn-default btnbranch"'.$btnunattr.'><i class="fa fa-minus-circle"></i>&ensp;Uninstall</a>';
 	} else {
 		$check = '';
 		$needspace = isset( $addon[ 'needspace' ] ) ? $addon[ 'needspace' ] : 1;
@@ -79,7 +79,7 @@ foreach( $arrayalias as $alias ) {
 			$btninattr = ' needspace="Need: <white>'.number_format( $needspace ).' MB</white><br>'.$available.$expandable.'"';
 		}
 		$btnin = '<a class="btn btn-default '.$btninclass.'"'.$btninattr.'>'.$buttonlabel.'</a>';
-		$btnun = '<a class="btn btn-default disabled"><i class="fa fa-minus-circle"></i> Uninstall</a>';
+		$btnun = '<a class="btn btn-default disabled"><i class="fa fa-minus-circle"></i>&ensp;Uninstall</a>';
 	}
 	
 	// addon list ---------------------------------------------------------------
