781012c452d9891a49bfbecfeb41099a0d7b35f8
rern <rernrern@gmail.com>
2018-07-06 14:14:16 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -14,7 +14,7 @@ restorefile /srv/http/app/templates/header.php /srv/http/app/templates/footer.ph
 echo -e "$bar Remove files ..."
 
 rm -v /srv/http/assets/css/addons*
-rm -v /srv/http/assets/fonts/addons.*,Inconsolata.*
+rm -v /srv/http/assets/fonts/{addons.*,Inconsolata.*}
 rm -v /srv/http/assets/js/addons*
 #rm -v /srv/http/assets/js/vendor/{hammer.min.js,propagating.js}
 rm -v /srv/http/addons*
