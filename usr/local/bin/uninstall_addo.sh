c7add0a96214e074ecbcc40b06e56d9b6396399d
rern <rernrern@gmail.com>
2018-07-23 21:11:14 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -16,10 +16,11 @@ echo -e "$bar Remove files ..."
 rm -v /srv/http/assets/css/addons*
 rm -v /srv/http/assets/fonts/{addons.*,Inconsolata.*}
 rm -v /srv/http/assets/js/addons*
-# must NOT remove - used by other addons
-#rm -v /srv/http/assets/js/vendor/jquery.mobile.custom.min.js
 rm -v /srv/http/addons*
 rm -r /srv/http/assets/addons
+
+# must NOT remove - used by other addons
+#rm -v /srv/http/assets/js/vendor/jquery.mobile.custom.min.js
 
 crontab -l | { cat | sed '/addonsupdate.sh/ d'; } | crontab -
 
