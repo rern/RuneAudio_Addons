791a8a9c675b76781f69ce01f1eccfdc9482320d
rern <rernrern@gmail.com>
2018-07-23 10:45:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,8 +53,7 @@ file=/srv/http/app/templates/footer.php
 echo $file
 
 string=$( cat <<'EOF'
-<script src="<?=$this->asset('/js/vendor/hammer.min.js')?>"></script>
-<script src="<?=$this->asset('/js/vendor/propagating.js')?>"></script>
+<script src="<?=$this->asset('/js/vendor/jquery.mobile.custom.min.js')?>"></script>
 <script src="<?=$this->asset('/js/addonsinfo.js')?>"></script>
 <script src="<?=$this->asset('/js/addonsmenu.js')?>"></script>
 EOF
