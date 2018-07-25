d66cb84a58c47705a95abc94cc7e01c71ee21e3b
rern <rernrern@gmail.com>
2018-07-25 17:21:08 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -6,17 +6,6 @@ $runeversion = ( $redis->get( 'release' ) == '0.4b' ) ? '0.4b' : '0.3';
 
 $redisaddons = $redis->hGetAll( 'addons' );
 
-$udaclist = $redis->hGetAll( 'udaclist' );
-if ( !$udaclist ) {
-	$acards = $redis->hGetAll( 'acards' );
-	foreach ( $acards as $key => $val ) {
-		$card = json_decode( $val, true );
-		$extlabel = $card[ 'extlabel' ];
-		$udaclist[ $extlabel ] = $key.'@'.$extlabel;
-		$redis->hSet( 'udaclist', $extlabel, $key.'@'.$extlabel );
-	}
-}
-
 ///////////////////////////////////////////////////////////////
 
 $addons = array(
@@ -469,7 +458,7 @@ $addons = array(
 	'option'       => array(
 		'radio'      => array(
 			'message'  => '<white>Audio output</white> when power off USB DAC:',
-			'list'     => $udaclist,
+			'list'     => $udaclist = $redis->hGetAll( 'udaclist' ),
 			'checked'  => 'bcm2835 ALSA_1@RaspberryPi Analog Out'
 
 		),
