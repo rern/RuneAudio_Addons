1032e0e09c5ef9a78b17e452d827ac9621af1823
rern <rernrern@gmail.com>
2018-08-02 14:47:15 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -382,8 +382,8 @@ $addons = array(
 	'sourcecode'   => 'https://github.com/rern/RuneAudio/tree/master/webradio',
 	'installurl'   => 'https://github.com/rern/RuneAudio/raw/master/webradio/webradiodb.sh',
 	'option'       => array(
-		'wait'       => 'Get webradio <code>*.pls</code> or <code>*.m3u</code> files or folders copied to:'
-					.'<br><code>/mnt/MPD/Webradio</code>'
+		'wait'       => 'Get webradio <code>*.pls</code> or <code>*.m3u</code> files or folders'
+					.'<br>copied to <code>/mnt/MPD/Webradio</code>'
 					.'<br>'
 					.'<br><code>&emsp;Ok&emsp;</code> to continue'
 	),
