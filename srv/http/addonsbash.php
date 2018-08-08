f8a99037acf17b71c5f8d00a29c0ed7c25a7243e
rern <rernrern@gmail.com>
2018-08-08 11:17:04 +0700

    Add files via upload

--- srv/http/addonsbash.php
+++ srv/http/addonsbash.php
@@ -4,24 +4,26 @@ ignore_user_abort( TRUE ); // for 'connection_status()' to work
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Addons</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="msapplication-tap-highlight" content="no" />
-    <link rel="stylesheet" href="assets/css/runeui.css">
-    <link rel="stylesheet" href="assets/css/addons.css">
-    <link rel="stylesheet" href="assets/css/addonsinfo.css">
-    <link rel="shortcut icon" href="assets/img/favicon.ico">
-</head>
+	<meta charset="utf-8">
+	<title>Addons</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="msapplication-tap-highlight" content="no" />
+	<link rel="stylesheet" href="assets/css/runeui.css">
+	<style>
+		@font-face {
+			font-family: addons;
+			src        : url( '/fonts/addons.woff' ) format( 'woff' ), url( '/fonts/addons.ttf' ) format( 'truetype' );
+			font-weight: normal;
+			font-style : normal;
+		}
+	</style>
+	<link rel="stylesheet" href="assets/css/addons.css">
+	<link rel="stylesheet" href="assets/css/addonsinfo.css">
+	<link rel="shortcut icon" href="assets/img/favicon.ico"></head>
 <body>
-
-<div id="loader" style="display: none;">
-	<div id="loaderbg"></div>
-	<div id="loadercontent"><i class="fa fa-addons"></i>connecting...</div>
-</div>
 
 <?php include 'addonslist.php';?>
 <!-- ...................................................................................... -->
