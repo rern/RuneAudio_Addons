a4a5fb070f158dbc19f124a8c83537ba14718d01
rern <rernrern@gmail.com>
2018-08-09 09:04:39 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -252,7 +252,7 @@ $addons = array(
 	'revision'     => 'Initial release',
 	'maintainer'   => 'r e r n',
 	'description'  => 'Add a separate <code><i class=\"fa fa-pause\"></i></code> button',
-	'thumbnail'    => '/assets/addons/thumbpaus.png',
+	'thumbnail'    => '/assets/addons/thumbpaus.gif',
 	'sourcecode'   => 'https://github.com/rern/RuneAudio/raw/master/pause_button',
 	'installurl'   => 'https://github.com/rern/RuneAudio/raw/master/pause_button/install.sh',
 	'hide'         => $redisaddons[ 'enha' ] ? 1 : 0,
