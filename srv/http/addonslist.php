16dd924cb58b89b511ee40648b89c97d503d71b0
rern <rernrern@gmail.com>
2018-08-10 14:13:05 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -144,16 +144,18 @@ $addons = array(
 	'option'       => array(
 		'radio'      => array(
 			'message'  => 'Download test for each server(seconds):',
-			'list'     => array(
-				'3'           => 3,
-				'4'           => 4,
-				'*5'          => 5,
-				'6'           => 6,
-				'7'           => 7,
-				'8'           => 8,
-				'9'           => 9,
-				'10'          => 10,
+			'message' => 'Download test for each server(seconds):',
+			'list'    => array(
+				'3'  => 3,
+				'4'  => 4,
+				'5'  => 5,
+				'6'  => 6,
+				'7'  => 7,
+				'8'  => 8,
+				'9'  => 9,
+				'10' => 10,
 			),
+			'checked' => 5,
 		),
 	),
 ),
