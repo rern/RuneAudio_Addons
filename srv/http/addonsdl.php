8b141a01e29b2937a426721610e9f0bb9baf6afb
rern <rernrern@gmail.com>
2018-08-22 18:24:31 +0700

    Add files via upload

--- srv/http/addonsdl.php
+++ srv/http/addonsdl.php
@@ -4,14 +4,32 @@ if ( isset( $_POST[ 'redis' ] ) ) {
 	$redis->pconnect( '127.0.0.1' );
 	$update = $redis->hGet( 'addons', $_POST[ 'redis' ] );
 	echo $update;
-} else if ( isset( $_FILES[ 'file' ] ) ) {
+} else if ( isset( $_POST[ 'backup' ] ) ) {  // settings backup only
+	$filename = 'backup_'.date( 'Ymd' ).'.tar.gz';
+	$file = '/srv/http/tmp/'.$filename;
+	$cmdlines = 'rm /srv/http/tmp/*'
+		.'; redis-cli save'
+		.' && bsdtar -czpf '.$file
+			.' --exclude /etc/netctl/examples'
+			.' /etc/netctl'
+			.' /mnt/MPD/Webradio'
+			.' /var/lib/redis/rune.rdb'
+			.' /var/lib/mpd'
+			.' /etc/mpd.conf'
+			.' /etc/mpdscribble.conf'
+			.' /etc/spop'
+		.'; echo $?'
+	;
+	$result = exec( $cmdlines );
+	echo $result == 0 ? '/tmp/'.$filename : false;
+} else if ( isset( $_FILES[ 'file' ] ) ) {  // settings restore  only
 	$filename = $_FILES[ 'file' ][ 'name' ];
 	$filetmp = $_FILES[ 'file' ][ 'tmp_name' ];
+	$filedest = '/srv/http/tmp/'.$filename;
 	$filesize = filesize( $filetmp );
-	
-	if ( !$filesize ) die();
+	if ( !$filesize ) die( '-1' );
 
-	echo move_uploaded_file( $filetmp, '/srv/http/tmp/'.$filename );
+	echo move_uploaded_file( $filetmp, $filedest );
 } else {
 	$branch = ( isset( $_GET[ 'branch' ] ) ) ? $_GET[ 'branch' ] : '';
 	exec( '/usr/bin/sudo /srv/http/addonsdl.sh '.$branch, $output, $exit );
