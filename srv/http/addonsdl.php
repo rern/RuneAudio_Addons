d9c808adcaca4caaf4d8f4c6957a4b476fb8fa05
rern <rernrern@gmail.com>
2018-08-22 10:56:07 +0700

    Add files via upload

--- srv/http/addonsdl.php
+++ srv/http/addonsdl.php
@@ -2,17 +2,20 @@
 if ( isset( $_POST[ 'redis' ] ) ) {
 	$redis = new Redis(); 
 	$redis->pconnect( '127.0.0.1' );
-
 	$update = $redis->hGet( 'addons', $_POST[ 'redis' ] );
 	echo $update;
-	die();
+} else if ( isset( $_FILES[ 'file' ] ) ) {
+	$filename = $_FILES[ 'file' ][ 'name' ];
+	$filetmp = $_FILES[ 'file' ][ 'tmp_name' ];
+	$filesize = filesize( $filetmp );
+	
+	if ( !$filesize ) die();
+
+	echo move_uploaded_file( $filetmp, '/srv/http/tmp/'.$filename );
+} else {
+	$branch = ( isset( $_GET[ 'branch' ] ) ) ? $_GET[ 'branch' ] : '';
+	exec( '/usr/bin/sudo /srv/http/addonsdl.sh '.$branch, $output, $exit );
+	// clear cache must be before echo
+	opcache_reset();
+	echo $exit;
 }
-
-$branch = ( isset( $_GET[ 'branch' ] ) ) ? $_GET[ 'branch' ] : '';
-
-exec( '/usr/bin/sudo /srv/http/addonsdl.sh '.$branch, $output, $exit );
-
-// clear cache must be before echo
-opcache_reset();
-
-echo $exit;
