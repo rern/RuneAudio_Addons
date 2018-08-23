424c8739c27a4e3b160076f3345423631039b638
rern <rernrern@gmail.com>
2018-08-23 09:00:31 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -26,10 +26,9 @@ rm -v /srv/http/app/templates/addons*
 rm -v /srv/http/assets/css/addons*
 rm -v /srv/http/assets/fonts/{addons*,Inconsolata*}
 rm -v /srv/http/assets/js/addons*
-rm -r /srv/http/tmp
-[[ $1 != u ]] && rm -r /srv/http/assets/img/addons
+rm -r /srv/http/assets/img/addons /srv/http/tmp
 
-# must NOT remove - used by other addons
+# DO NOT remove - used by other addons
 #rm -v /srv/http/assets/js/vendor/jquery.mobile.custom.min.js
 
 crontab -l | { cat | sed '/addonsupdate.sh/ d'; } | crontab -
