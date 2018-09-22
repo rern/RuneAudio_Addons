0765b2df303c37d42ee9c593d98f09f1c22b1906
rern <rernrern@gmail.com>
2018-09-22 15:34:39 +0700

    Update ui_reset.sh

--- usr/local/bin/ui_reset.sh
+++ usr/local/bin/ui_reset.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+[[ ! -e /srv/http/addonstitle.sh ]] && wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/addonstitle.sh -P /srv/http
 . /srv/http/addonstitle.sh
 
 yesno "All RuneUI addons and custom UI modifications will be removed.\nContinue?" answer
