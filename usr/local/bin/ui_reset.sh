6a7bfa5c83413227d84ba299a36ee3b5e8510cb3
rern <rernrern@gmail.com>
2018-09-22 15:12:09 +0700

    Update ui_reset.sh

--- usr/local/bin/ui_reset.sh
+++ usr/local/bin/ui_reset.sh
@@ -2,12 +2,10 @@
 
 . /srv/http/addonstitle.sh
 
-if [[ -t 1 ]]; then
-	yesno "All RuneUI addons and custom UI modifications will be removed.\nContinue?" answer
-	if [[ $answer == 0 ]]; then
-		title "$info RuneUI Reset cancelled."
-		exit
-	fi
+yesno "All RuneUI addons and custom UI modifications will be removed.\nContinue?" answer
+if [[ $answer == 0 ]]; then
+	title "$info RuneUI Reset cancelled."
+	exit
 fi
 
 wgetnc https://github.com/rern/RuneAudio/raw/master/ui_reset/reset.sh
