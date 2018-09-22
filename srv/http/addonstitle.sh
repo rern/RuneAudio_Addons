3b5c343854b08bf920def21f678776c3b6765132
rern <rernrern@gmail.com>
2018-09-22 16:26:50 +0700

    Update addonstitle.sh

--- srv/http/addonstitle.sh
+++ srv/http/addonstitle.sh
@@ -269,7 +269,6 @@ uninstallfinish() { # $1-'u'=update
 	title -l '=' "$bar $title uninstalled successfully."
 }
 clearcache() {
-	[[ -t 1 ]] && systemctl reload php-fpm
 	title -nt "$bar Restart local browser ..."
 	if pgrep Xorg > /dev/null; then
 		killall Xorg
