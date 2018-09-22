12aa6cb01358a83608c6faf3ca7bf4da80a2fe73
rern <rernrern@gmail.com>
2018-09-22 17:53:31 +0700

    Add files via upload

--- srv/http/addonsdl.php
+++ srv/http/addonsdl.php
@@ -33,7 +33,5 @@ if ( isset( $_POST[ 'redis' ] ) ) {
 } else {
 	$branch = ( isset( $_GET[ 'branch' ] ) ) ? $_GET[ 'branch' ] : '';
 	exec( '/usr/bin/sudo /srv/http/addonsdl.sh '.$branch, $output, $exit );
-	// clear cache must be before echo
-	opcache_reset();
 	echo $exit;
 }
