d348dedb3e318cdf1915f9449f5762dde696087a
rern <rernrern@gmail.com>
2018-09-24 09:47:31 +0700

    Add files via upload

--- usr/local/bin/ui_reset.sh
+++ usr/local/bin/ui_reset.sh
@@ -3,11 +3,8 @@
 [[ ! -e /srv/http/addonstitle.sh ]] && wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/master/srv/http/addonstitle.sh -P /srv/http
 . /srv/http/addonstitle.sh
 
-yesno "All RuneUI addons and custom UI modifications will be removed.\nContinue?" answer
-if [[ $answer == 0 ]]; then
-	title "$info RuneUI Reset cancelled."
-	exit
-fi
+yesno "All RuneUI addons and custom UI modifications will be removed.\nContinue?"
+[[ $answer == 0 ]] && title "$info RuneUI Reset cancelled." && exit
 
 wgetnc https://github.com/rern/RuneAudio/raw/master/ui_reset/reset.sh
 chmod +x reset.sh
