6f25a077db6ad267aa7ca1c956946017eca479a5
rern <rernrern@gmail.com>
2019-01-24 17:09:32 +0700

    Update uninstall_addo.sh

--- usr/local/bin/uninstall_addo.sh
+++ usr/local/bin/uninstall_addo.sh
@@ -11,6 +11,7 @@ echo -e "$bar Restore files ..."
 
 files="
 /srv/http/index.php
+/srv/http/index.php.backup
 /srv/http/app/templates/settings.php
 /srv/http/app/templates/header.php
 /srv/http/app/templates/footer.php
