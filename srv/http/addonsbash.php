64520ecb45ce92bd2c00babb2847d1d23855640f
rern <rernrern@gmail.com>
2019-01-26 09:09:00 +0700

    Add files via upload

--- srv/http/addonsbash.php
+++ srv/http/addonsbash.php
@@ -12,7 +12,8 @@ $time = time();
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="msapplication-tap-highlight" content="no" />
+	<meta name="msapplication-tap-highlight" content="no">
+	<link rel="shortcut icon" href="/img/favicon.<?=$time?>.ico">
 	<style>
 		@font-face {
 			font-family: addons;
