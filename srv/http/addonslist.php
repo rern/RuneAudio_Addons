a722a079f3f9f96db245d61127e59a26a8cbbebf
rern <rernrern@gmail.com>
2019-02-21 14:30:47 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -37,8 +37,12 @@ $addons = array(
 */
 'enha' => array(
 	'title'       => 'RuneUI Enhancement **',
-	'version'     => '20190213',
-	'revision'    => 'Fix bug - add/replace from Album, Artist, Composer and Genre'
+	'version'     => '20190221',
+	'revision'    => 'Reinstate coverart bookmarks (long delay fixed)'
+					.'<br>Fix - Library blocks dragging to rearrange'
+					.'<br>Fix - Library coverart context menu'
+					.'<br>...'
+					.'<br>Fix bug - add/replace from Album, Artist, Composer and Genre'
 					.'<br>Fix missing Back button'
 					.'<br>Fix Rescan button'
 					.'<br>Improve scrolling texts to the same speed, 80px/s'
@@ -46,12 +50,7 @@ $addons = array(
 					.'<br>Fix missing metadata on initial load'
 					.'<br>Fix inaccessible Webradio list when disable counting in Library'
 					.'<br>Fix missing back icon'
-					.'<br>Bookmarks can be dragged to rearrange order'
-					.'<br>...'
-					.'<br>Fix search bug'
-					.'<br>Fix delay on initial page loading with large Library'
-					.'<br>Improve text scrolling in scaled landscape mode on small screen'
-					.'<br>Add option to disable open Playback on Add+Play',
+					.'<br>Bookmarks can be dragged to rearrange order',
 	'maintainer'  => 'r e r n',
 	'description' => 'Lots of new features. More <white>minimalism</white> and more <white>fluid</white> layout.',
 	'thumbnail'   => '/img/addons/thumbenha.gif',
