e3e2536e2994e724dd133979a76a53ff075a2ce2
rern <rernrern@gmail.com>
2019-05-10 07:41:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -125,7 +125,7 @@ systemctl enable addons cronie
 systemctl start addons cronie
 
 # fix missing data in 0.5
-[[ -z $acards ]] && /srv/http/command/refresh_ao
+[[ -z $( redis-cli hgetall acards ) ]] && /srv/http/command/refresh_ao
 
 # for backup file upload
 dir=/srv/http/tmp
