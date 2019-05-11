fef3f76a3ccf59005c6a1ba1a77667d05fb4e9e6
rern <rernrern@gmail.com>
2019-05-11 16:55:56 +0700

    Add files via upload

--- srv/http/addonsdl.sh
+++ srv/http/addonsdl.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 # existing 'addonslist.php'
-addonslist=$( sed -n "/'addo'/,/^),/p" /srv/http/addonslist.php )
-installurl=$( echo "$addonslist" | grep 'installurl.*=>' | cut -d "'" -f 4 )
-gitpath=$( dirname $installurl )
 branch=master
+installurl=$( grep 'RuneAudio_Addons/raw/master/install.sh' /srv/http/addonslist.php | cut -d "'" -f 4 )
 
-# for testing branch: $1=branchname
+# for testing branch: $1=branch
 if (( $# != 0 )); then
 	branch=$1
-	gitpath=$( echo $( dirname $gitpath )/$branch ) # switch 'master' to 'branch'
-	installurl=$gitpath/install.sh
+	installurl=$( echo $installurl | sed s"/master/$branch/" )
 fi
 
-wget -qN --no-check-certificate $gitpath/srv/http/addonslist.php -P /srv/http
+# minor fixes
+wget -qN --no-check-certificate $( dirname $installurl )/update.sh
+if [[ $? == 0 ]]; then
+	chmod +x update.sh
+	./update.sh
+	rm update.sh
+fi
+
+wget -qN --no-check-certificate $( dirname $installurl )/srv/http/addonslist.php -P /srv/http
 [[ $? != 0 ]] && exit 1
 
-# new 'addonslist.php'
-addonslist=$( sed -n "/'addo'/,/^),/p" /srv/http/addonslist.php )
-
-versionlist=$( echo "$addonslist" | grep 'version.*=>' | cut -d "'" -f 4 )
+versionlist=$( sed -n "/^'addo/ {n;n;p}" /srv/http/addonslist.php | cut -d "'" -f 4 )
 versionredis=$( redis-cli hget addons addo )
 
 if [[ $versionlist > $versionredis ]]; then
@@ -38,25 +40,5 @@ if [[ $versionlist > $versionredis ]]; then
 fi
 
 . /srv/http/addonsupdate.sh 1
-
-# get settings: notify pointer zoom
-file=/etc/X11/xinit/start_chromium.sh
-if [[ ! -e $file ]]; then
-	if ! grep '^chromium' /root/.xinitrc; then
-		zoom=$( grep '^zoom-level' /root/.config/midori/config | cut -d'=' -f2 )
-	else
-		zoom=$( grep 'force-device-scale-factor' /root/.xinitrc | cut -d'=' -f3 )
-	fi
-	file=/root/.xinitrc
-else
-	zoom=$( grep 'force-device-scale-factor' /etc/X11/xinit/start_chromium.sh | cut -d'=' -f3 )
-fi
-pointer=$( grep 'use_cursor' $file | cut -d' ' -f5 )
-if [[ -e /srv/http/assets/js/enhance.js ]]; then
-	notify=$( grep 'PNotify.prototype.options.delay' /srv/http/assets/js/enhance.js | cut -d' ' -f3 | tr -d '0;' )
-else
-	notify=$( grep 'notify.delay' /srv/http/assets/js/runeui.js | cut -d'?' -f2 | cut -d' ' -f2 )
-fi
-redis-cli hmset settings notify $notify pointer $pointer zoom $zoom &> /dev/null
 
 exit 0 # force exit code = 0
