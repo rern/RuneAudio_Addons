02d7c8aacc6a6a4926cb388e17c61e894f5b4ebc
rern <rernrern@gmail.com>
2019-05-13 14:19:38 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 
 [[ -e /srv/http/assets/img/addons/addons.png ]] && exit
 
-wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/css/addonsinfo.css -O /srv/http/assets/css
-wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/img/addons/addons.png -O /srv/http/assets/img/addons
-wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/js/addonsinfo.js -O /srv/http/assets/js
+file=/srv/http/assets/css/addonsinfo.css
+wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE$file -O $file
+file=/srv/http/assets/img/addons/addons.png
+wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE$file -O $file
+file=/srv/http/assets/jsaddonsinfo.js
+wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE$file -O $file
