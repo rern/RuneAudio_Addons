d3970eb7c60a0c030b24a3a8f3476f002e49949c
rern <rernrern@gmail.com>
2019-05-17 11:45:57 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-[[ -e /srv/http/assets/img/addons/addons.png ]] && exit
-
-file=/srv/http/assets/css/addonsinfo.css
-wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE$file -O $file
-file=/srv/http/assets/img/addons/addons.png
-wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE$file -O $file
-file=/srv/http/assets/js/addonsinfo.js
-wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE$file -O $file
+if [[ -e /srv/http/assets/img/addons/addons.png ]]; then
+	file=/srv/http/assets/js/addonsinfo.js
+	[[ $( grep cancelbutton $file ) ]] && wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE$file -O $file
+else
+	file=/srv/http/assets/css/addonsinfo.css
+	wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE$file -O $file
+	file=/srv/http/assets/img/addons/addons.png
+	wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE$file -O $file
+	file=/srv/http/assets/js/addonsinfo.js
+	wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE$file -O $file
+fi
