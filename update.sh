c434c7d0d3599eb6c71fc6cc96f92d6d5d1c7dd1
rern <rernrern@gmail.com>
2019-05-21 20:07:35 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 file=/srv/http/assets/css/addonsinfo.css
-grep -q 34px $file && exit
+! grep -q 34px $file && wget https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/css/addonsinfo.css -O $file
 
-wget https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/css/addonsinfo.css -O $file
+file=/srv/http/assets/js/addons.js
+! grep -q dual $file && wget https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/js/addons.js -O $file
