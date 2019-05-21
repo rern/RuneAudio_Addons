060d03ac7410a1eee7cc652b94e998a8a9d1cc97
rern <rernrern@gmail.com>
2019-05-21 13:16:09 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+file=/srv/http/assets/css/addonsinfo.css
+[[ $( sed -n '/infoIcon/ {n;p}' $file | tr -d '\t' ) == 'float: left;' ]] && exit
+
 sed -i -e '/infoIcon/ {n;d}
 ' -e '/vertical-align: 12px/ i\
 	float: left;
-' /srv/http/assets/css/addonsinfo.css
+' $file
