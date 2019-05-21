13033b4f77fb6322034da1376af1d6527fd48b33
rern <rernrern@gmail.com>
2019-05-21 13:34:18 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,4 @@
 file=/srv/http/assets/css/addonsinfo.css
 [[ $( sed -n '/infoIcon/ {n;p}' $file | tr -d '\t' ) == 'float: left;' ]] && exit
 
-sed -i -e '/infoIcon/ {n;d}
-' -e '/vertical-align: 12px/ i\
-	float: left;
-' $file
+wget https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/css/addonsinfo.css -O $file
