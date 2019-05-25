1660cb011d76fb98831cd858c65ba6c7771f5fcd
rern <rernrern@gmail.com>
2019-05-25 08:42:35 +0700

    Delete update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-file=/srv/http/assets/css/addonsinfo.css
-! grep -q 38px $file && wget https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/css/addonsinfo.css -O $file
-
-file=/srv/http/assets/js/addons.js
-! grep -q dual $file && wget https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/js/addons.js -O $file
