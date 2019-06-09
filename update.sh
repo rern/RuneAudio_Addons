517f28c57d00a7a77e3b590b66e852e4e872db98
rern <rernrern@gmail.com>
2019-06-09 15:43:43 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,5 +2,8 @@
 
 if ! grep -q flushdot /srv/http/addonsbash.php; then
 	wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/addonsbash.php -P /srv/http
-  wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/css/addons.css -P /srv/http/assets/css
+	wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/assets/css/addons.css -P /srv/http/assets/css
+fi
+if ! grep -q packagestatus /srv/http/addonstitle.sh; then
+	wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/addonstitle.sh
 fi
