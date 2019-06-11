c5e68e6e69d52daac294d3fdd811e39f09d22a08
rern <rernrern@gmail.com>
2019-06-11 09:23:54 +0700

    Update addonstitle.sh

--- srv/http/addonstitle.sh
+++ srv/http/addonstitle.sh
@@ -136,7 +136,7 @@ getvalue() { # $1-key
 rankmirrors() {
 	now=$( date '+%s' )
 	timestamp=$( date -r /etc/pacman.d/mirrorlist '+%s' )
-	if (( $(( now - timestamp )) > 86400 )); then
+	if (( $(( now - timestamp )) > 3600 )); then # only if more than 1 hour
 		wgetnc https://github.com/rern/RuneAudio/raw/master/rankmirrors/rankmirrors.sh
 		chmod +x rankmirrors.sh
 		./rankmirrors.sh
