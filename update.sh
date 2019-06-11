c174300f6d5898b687e26eacccb17189faebe40d
rern <rernrern@gmail.com>
2019-06-11 08:09:36 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if grep -q 'installed=0' /srv/http/addonstitles.sh; then
+if grep -q 'installed=0' /srv/http/addonstitle.sh; then
   wget -qN --no-check-certificate https://github.com/rern/RuneAudio_Addons/raw/UPDATE/srv/http/addonstitle.sh -P /srv/http
   chmod +x /srv/http/addonstitle.sh
 fi
