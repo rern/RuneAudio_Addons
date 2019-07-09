139e1119d94ceb8b3e79be6ce1532b41b1a8ebd8
rern <rernrern@gmail.com>
2019-07-09 18:43:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,23 +24,29 @@ alias=addo
 
 installstart $@
 
-if [[ $( redis-cli get release ) == '0.5' ]]; then # for 0.4 pacman
-	packagestatus openssl-cryptodev # $version, $installed
-	if [[ -z $installed ]]; then
-		echo -e "$bar Upgrade common packages ..."
+# for 0.4
+cp /usr/lib/libcrypto.so.1.0.0{,.backup} &> /dev/null
+cp /usr/lib/libssl.so.1.0.0{,.backup} &> /dev/null
 
-		rankmirrors
-		echo -e "\n$bar Upgrade OpenSSL ..."
-		echo -e "y \n" | pacman -S openssl-cryptodev
-	fi
+packagestatus openssl-cryptodev # $version, $installed
+if [[ -z $installed ]]; then
+	echo -e "$bar Upgrade common packages ..."
 
-	packagestatus glibc
-	if [[ -z $installed ]]; then
-		rankmirrors
-		echo -e "\n$bar GNU C Library ..."
-		pacman -S --noconfirm glibc freetype2
-	fi
+	rankmirrors
+	echo -e "\n$bar Upgrade OpenSSL ..."
+	echo -e "y \n" | pacman -S openssl-cryptodev
 fi
+
+packagestatus glibc
+if [[ -z $installed ]]; then
+	rankmirrors
+	echo -e "\n$bar GNU C Library ..."
+	pacman -S --noconfirm glibc freetype2
+fi
+
+# for 0.4
+cp /usr/lib/libcrypto.so.1.0.0{.backup,} &> /dev/null
+cp /usr/lib/libssl.so.1.0.0{.backup,} &> /dev/null
 
 getinstallzip
 
