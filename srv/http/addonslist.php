937935faf2e9f8fae8daed6caf8fdc8a0b8b11e6
rern <rernrern@gmail.com>
2019-08-24 02:51:38 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -5,31 +5,8 @@ $runee1 = file_exists( '/srv/http/startup.sh' );
 // temp
 if ( $runee1 && !$redis->hExists( 'addons', 'rre1' ) ) $redis->hSet( 'addons', 'rre1', 20190822 );
 
-$rune05 = $redis->get( 'release' ) === '0.5';
 $runee1 = file_exists( '/srv/http/startup.sh' );
 $redisaddons = $redis->hGetAll( 'addons' );
-$enha = $redisaddons[ 'enha' ];
-// checked items
-$enhacheck = array();
-if ( $redis->hGet( 'mpdconf', 'ffmpeg' ) === 'yes' ) $enhacheck[] = 0;
-if ( $redis->hGet( 'AccessPoint', 'enabled' ) == 1 ) $enhacheck[] = 1;
-if ( ( $rune05 && $redis->hget( 'local_browser', 'enable' ) == 1 )
-		|| $redis->get( 'local_browser' ) == 1
-) {
-	$enhacheck[] = 2;
-	$localbrowser = 1;
-}
-if ( $redis->hGet( 'airplay', 'enable' ) == 1 ) $enhacheck[] = 3;
-if ( $redis->hGet( 'dlna', 'enable' ) == 1 ) $enhacheck[] = 4;
-
-$acards = $redis->hGetAll( 'acards' );
-$udaclist = array(); // fix: undefined $udaclist error( empty $acards)
-foreach( $acards as $key => $value ) {
-	$value = json_decode( $value );
-	$name = $value->extlabel ?: $value->name;
-	$udaclist[ $name ] = $key;
-}
-ksort( $udaclist );
 
 ///////////////////////////////////////////////////////////////
 $addons = array(
@@ -57,55 +34,6 @@ $addons = array(
 	'nouninstall' => 1,
 	'sourcecode'  => 'https://github.com/rern/RuneAudio-Re1',
 	'installurl'  => 'https://github.com/rern/RuneAudio-Re1/raw/master/update.sh',
-	'hide'        => !$runee1
-),
-'enha' => array(
-	'title'       => 'RuneUI Enhancement **',
-	'version'     => '20190709',
-	'revision'    => 'General improvements'
-					.'<br>...'
-					.'<br>Fix replace/remove/save coverart bug'
-					.'<br>Fix volume not update on other devices'
-					.'<br>Fix albums/artists with % or # not show in Browse By CoverArt'
-					.'<br>Much faster loading long saved playlists'
-					.'<br>...'
-					.'<br>Fix add webradio bug'
-					.'<br>Fix playlists conversion during install'
-					.'<br>Fix playlists with 1,000 tracks not show'
-					.'<br>Fix saved playlists with 1,000 tracks not load'
-					.'<br>Reinstate Last.fm as primary source for online coverarts',
-	'maintainer'  => 'r e r n',
-	'description' => 'Lots of new features. More <w>minimalism</w> and more <w>fluid</w> layout.',
-	'thumbnail'   => '/img/addons/thumbenha.gif',
-	'sourcecode'  => 'https://github.com/rern/RuneUI_enhancement',
-	'installurl'  => 'https://github.com/rern/RuneUI_enhancement/raw/master/install.sh',
-	'option'      => array(
-		'radio'     => array(
-			'message' => 'Set <w>zoom level</w> for display directly connect to RPi.'
-						.'<br>(This can be changed later.)'
-						.'<br>Local screen size:',
-			'list'    => array(
-				'Width less than 800px: 0.7' => 0.7,
-				'HD - 1280px: 1.2'           => 1.2,
-				'Full HD - 1920px: 1.5'      => 1.5,
-				'Full HD - no buttons: 1.8'  => 1.8,
-				'Custom'                     => '?'
-			),
-			'checked' => 1.8
-		),
-		'checkbox'  => array(
-			'message' => 'Should be unchecked if not used:',
-			'list'    => array(
-				'<gr>Enable</gr> AAC/ALAC'      => 1,
-				'<gr>Enable</gr> Access point'  => 1,
-				'<gr>Enable</gr> Local browser' => 1,
-				'<gr>Enable</gr> AirPlay'       => 1,
-				'<gr>Enable</gr> UPnP/DLNA'     => 1
-			),
-			'checked' => $enhacheck
-		),
-	),
-	'hide'        => $runee1
 ),
 'extr' => array(
 	'title'       => 'RuneAudio <i class="fa fa-addons"></i> e1 - Restore Extra Directories',
@@ -114,7 +42,6 @@ $addons = array(
 	'buttonlabel' => 'Restore',
 	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/extradir',
 	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/extradir/install.sh',
-	'hide'        => !$enha || $runee1
 ),
 'rrre' => array(
 	'title'       => 'RuneAudio <i class="fa fa-addons"></i> e1 Reset',
@@ -123,7 +50,6 @@ $addons = array(
 	'buttonlabel' => 'Reset',
 	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/RuneAudio%2BRuneUIe.img/',
 	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/RuneAudio%2BRuneUIe.img/setup.sh',
-	'hide'        => !$runee1
 ),
 'cove' => array(
 	'title'       => 'Browse By CoverArt Thumbnails',
@@ -209,7 +135,6 @@ $addons = array(
 	'thumbnail'   => '/img/addons/thumbgpio.gif',
 	'sourcecode'  => 'https://github.com/rern/RuneUI_GPIO',
 	'installurl'  => 'https://github.com/rern/RuneUI_GPIO/raw/master/install.sh',
-	'hide'        => !$runee1
 ),
 'tran' => array(
 	'title'       => 'Transmission *',
@@ -237,19 +162,6 @@ $addons = array(
 			'checked' => array( 0, 1 )
 		),
 	),
-),
-'back' => array(
-	'title'       => 'Settings+Databases Backup',
-	'revision'    => 'Initial release',
-	'maintainer'  => 'r e r n',
-	'description' => 'Backup all RuneAudio <w>settings and databases</w>.',
-	'thumbnail'   => '/img/addons/thumbback.png',
-	'buttonlabel' => 'Backup',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/backup-restore',
-	'option'      => array(
-		'confirm'   => 'Backup all RuneAudio <w>settings and databases</w>?',
-	),
-	'hide'        => $runee1
 ),
 
 );
