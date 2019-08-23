4735d1cd78dec9ae1fe4cc2f09847b0133467215
rern <rernrern@gmail.com>
2019-08-24 02:45:47 +0700

    Update addonslist.php

--- srv/http/addonslist.php
+++ srv/http/addonslist.php
@@ -31,63 +31,6 @@ foreach( $acards as $key => $value ) {
 }
 ksort( $udaclist );
 
-// samba
-if ( exec( '/usr/bin/pacman -Qs mpd-rune' ) ) {
-	$optmpd = array(
-		'confirm'   => 'Once installed, MPD <w>cannot be downgraded</w>.'
-					  .( $rune05 ? '' : '<br>Midori, local browser, must be upgrade as well.' )
-	);
-} else {
-	$optmpd = array(
-		'confirm'  => 'Upgrade?'
-	);
-}
-// chromium
-if ( file_exists( '/usr/bin/chromium' ) ) {
-	$xinitrc = file_get_contents( '/etc/X11/xinit/xinitrc' );
-	if ( !preg_match( '/calibrator/', $xinitrc ) ) {
-		$chromiumfile = '/etc/X11/xinit/xinitrc';
-	} else {
-		$chromiumfile = '/etc/X11/xinit/start_chromium.sh';
-		$chromiumfile = file_exists( $chromiumfile ) ? $chromiumfile : '/root/.xinitrc';
-	}
-	$zoom = exec( "/usr/bin/sudo /usr/bin/grep 'chromium --' $chromiumfile | /usr/bin/cut -d'=' -f3" );
-	$chromium = 1;
-} else {
-	$zoom = exec( "/usr/bin/sudo /usr/bin/grep '^zoom' /root/.config/midori/config | /usr/bin/cut -d'=' -f2" );
-}
-// samba
-if ( exec( '/usr/bin/pacman -Qs samba4-rune' ) ) {
-	$optsamb = array(
-		'password'  => array(
-			'message' => '(for connecting to <w>USB root share</w>)'
-						.'<br>Password for user <w>root</w> (blank = existing or rune):',
-			'label'   => 'Password'
-		),
-		'skip'      => 'Keep current Samba settings and shares?',
-		'wait'      => 'Connect a <w>USB drive</w> before continue.'
-					  .'<br>1st drive will be used for shared directories.',
-		'text1'     => array(
-			'message' => '<w>File Server</w>:',
-			'label'   => 'Name',
-			'value'   => 'RuneAudio'
-		),
-		'text2'     => array(
-			'message' => '<w>Read-Only</w> directory:',
-			'label'   => 'Name',
-			'value'   => 'ro'
-		),
-		'text3'     => array(
-			'message' => '<w>Read-Write</w> directory:',
-			'label'   => 'Name',
-			'value'   => 'rw'
-		),
-	);
-} else {
-	$optsamb = array(
-		'confirm'  => 'Upgrade?'
-	);
-}
 ///////////////////////////////////////////////////////////////
 $addons = array(
 
@@ -165,7 +108,7 @@ $addons = array(
 	'hide'        => $runee1
 ),
 'extr' => array(
-	'title'       => 'RuneUIe - Restore Extra Directories',
+	'title'       => 'RuneAudio <i class="fa fa-addons"></i> e1 - Restore Extra Directories',
 	'maintainer'  => 'r e r n',
 	'description' => 'Restore extra directories: bookmarks, coverarts, lyrics, playlists and webradios.',
 	'buttonlabel' => 'Restore',
@@ -187,17 +130,6 @@ $addons = array(
 	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/coverarts/scan.sh',
 	'hide'        => 1,
 ),
-'kid3' => array(
-	'title'       => 'RuneUIe Metadata Tag Editor',
-	'depend'      => 'enha',
-	'needspace'   => 250,
-	'revision'    => 'Initial release',
-	'maintainer'  => 'r e r n',
-	'description' => 'Enable metadata editor feature in context menu.',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/kid3-cli',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/kid3-cli/install.sh',
-	'hide'        => $runee1
-),
 'aria' => array(
 	'title'       => 'Aria2 *',
 	'version'     => '20170901',
@@ -215,25 +147,6 @@ $addons = array(
 		)
 	),
 ),
-'chro' => array(
-	'title'       => 'Chromium Upgrade',
-	'needspace'   => 200,
-	'revision'    => 'Purge before install for Chromium 75.0'
-					.'<br>...'
-					.'<br>Fix missing packages - Chromium 65.0.3325.181-1'
-					.'<br>...'
-					.'<br>Fix symbol lookup errors in new update.',
-	'maintainer'  => 'r e r n',
-	'description' => 'A local browser replacement. RuneAudio 0.4 needs MPD Upgrade before install.',
-	'thumbnail'   => '/img/addons/thumbchro.png',
-	'buttonlabel' => 'Upgrade',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/chromium',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/chromium/install.sh',
-	'option'      => array(
-		'confirm'   => 'Upgrade?'
-	),
-	'hide'        => !$rune05 && !$runee1
-),
 'dual' => array(
 	'title'       => 'Dual Boot: RuneAudio + OSMC *',
 	'maintainer'  => 'r e r n',
@@ -243,42 +156,6 @@ $addons = array(
 	'thumbnail'   => '/img/addons/thumbdual.gif',
 	'buttonlabel' => 'Link',
 	'installurl'  => 'http://www.runeaudio.com/forum/dual-boot-noobs-rune-osmc-pi2-pi3-t3822.html',
-),
-'expa' => array(
-	'title'       => 'Expand Partition',
-	'maintainer'  => 'r e r n',
-	'description' => 'Expand default 2GB partition to full capacity of SD card.',
-	'thumbnail'   => '/img/addons/thumbpart.png',
-	'buttonlabel' => 'Expand',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/expand_partition',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/expand_partition/expand.sh',
-	'hide'        => $redisaddons[ 'expa' ] || $runee1
-),
-'motd' => array(
-	'title'       => 'Login Logo for Terminal',
-	'version'     => '20180115',
-	'revision'    => 'Select color on install',
-	'maintainer'  => 'r e r n',
-	'description' => 'Message of the day - RuneAudio Logo and dimmed command prompt.',
-	'thumbnail'   => '/img/addons/thumbmotd.png',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/motd',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/motd/install.sh',
-	'option'      => array(
-		'radio'     => array(
-			'message' => 'Set logo color:',
-			'list'    => array(
-				'<a style="color: #f#0095d8">UI blue</a>' => 0,
-				'<a style="color: #ff0000">Red</a>'       => 1,
-				'<a style="color: #00ff00">Green'         => 2,
-				'<a style="color: #ffff00">Yellow'        => 3,
-				'<a style="color: #ff00ff">Magenta'       => 5,
-				'<a style="color: #00ffff">Cyan'          => 6,
-				'<a style="color: #ffffff">White'         => 7,
-			),
-			'checked' => 0
-		),
-	),
-	'hide'        => $runee1
 ),
 'mpdu' => array(
 	'title'       => 'MPD Upgrade',
@@ -291,7 +168,6 @@ $addons = array(
 	'buttonlabel' => 'Upgrade',
 	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/mpd',
 	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/mpd/install.sh',
-	'option'      => $optmpd,
 ),
 'rank' => array(
 	'title'       => 'Rank Mirror Package Servers',
@@ -319,18 +195,6 @@ $addons = array(
 		),
 	),
 ),
-'font' => array(
-	'title'       => 'RuneUI Fonts - Extended Characters',
-	'version'     => '20170901',
-	'needspace'   => 9,
-	'revision'    => 'Initial release',
-	'maintainer'  => 'r e r n',
-	'description' => 'Font files replacement for Extended Latin-based, Cyrillic-based, Greek and IPA phonetics.',
-	'thumbnail'   => '/img/addons/thumbfont.png',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/font_extended',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/font_extended/install.sh',
-	'hide'        => $rune05 || $runee1
-),
 'gpio' => array(
 	'title'       => 'RuneUI GPIO *',
 	'version'     => '20190824',
@@ -346,66 +210,6 @@ $addons = array(
 	'sourcecode'  => 'https://github.com/rern/RuneUI_GPIO',
 	'installurl'  => 'https://github.com/rern/RuneUI_GPIO/raw/master/install.sh',
 	'hide'        => !$runee1
-),
-'lyri' => array(
-	'title'       => 'RuneUI Lyrics',
-	'version'     => '20190701',
-	'revision'    => 'Link saved location to common directory'
-					.'<br>...'
-					.'<br>Allow colors changed with RuneUIe'
-					.'<br>...'
-					.'<br>Fix - Blank page if RuneUI Enhancement uninstalled',
-	'maintainer'  => 'r e r n',
-	'description' => 'Improve lyrics feature in 0.4b / add lyrics feature in 0.3',
-	'thumbnail'   => '/img/addons/thumblyri.gif',
-	'sourcecode'  => 'https://github.com/RuneAddons/Lyrics',
-	'installurl'  => 'https://github.com/RuneAddons/Lyrics/raw/master/install.sh',
-	'hide'        => $runee1
-),
-'paus' => array(
-	'title'       => 'RuneUI Pause Button',
-	'version'     => '20180217',
-	'revision'    => 'Initial release',
-	'maintainer'  => 'r e r n',
-	'description' => 'Add a separate <code><i class=\"fa fa-pause\"></i></code> button',
-	'thumbnail'   => '/img/addons/thumbpaus.gif',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/pause_button',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/pause_button/install.sh',
-	'hide'        => $enha || $runee1
-),
-'uire' => array(
-	'title'       => 'RuneUI Reset',
-	'maintainer'  => 'r e r n',
-	'description' => 'Remove all installed addons and reset RuneUI to default with Addons reinstalled.'
-					.'It can be used as an alternative to reflashing the SD card.',
-	'buttonlabel' => 'Reset',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/ui_reset',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/ui_reset/reset.sh',
-	'option'      => array(
-		'confirm'   => 'All RuneUI addons and custom UI modifications'
-					  .'<br><w>will be removed</w>.'
-	),
-	'hide'        => $runee1
-),
-'samb' => array(
-	'title'       => 'Samba Upgrade',
-	'needspace'   => 43,
-	'maintainer'  => 'r e r n',
-	'description' => 'Faster and more customized shares.',
-	'thumbnail'   => '/img/addons/thumbsamb.png',
-	'buttonlabel' => 'Upgrade',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/samba',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/samba/install.sh',
-	'option'      => $optsamb
-),
-'shai' => array(
-	'title'       => 'Shairport Sync Upgrade',
-	'maintainer'  => 'r e r n',
-	'description' => 'Less configuration.',
-	'buttonlabel' => 'Upgrade',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/shairport-sync',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/shairport-sync/install.sh',
-	'hide'        => 1
 ),
 'tran' => array(
 	'title'       => 'Transmission *',
@@ -434,155 +238,6 @@ $addons = array(
 		),
 	),
 ),
-'udac' => array(
-	'title'       => 'USB DAC Plug and Play',
-	'version'     => '20190422',
-	'revision'    => 'Minor improvements'
-					.'<br>...'
-					.'<br>Fix bugs when running on 0.5'
-					.'<br>...'
-					.'<br><w>Audio output</w> can be selected for preset when power off USB DAC',
-	'maintainer'  => 'r e r n',
-	'description' => 'Automatically switch to/from MPD Audio output and reload configuration:'
-					.'<br>- USB DAC <w>power on</w> - switch to <w>USB DAC</w>'
-					.'<br>- USB DAC <w>power off</w> - switch to <w>preset Audio output</w>',
-	'thumbnail'   => '/img/addons/thumbudac.png',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/USB_DAC',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/USB_DAC/install.sh',
-	'option'      => array(
-		'radio'     => array(
-			'message' => '<w>Audio output</w> when power off USB DAC:',
-			'list'    => $udaclist,
-			'checked' => $redis->get( 'ao' )
-		),
-	),
-	'hide'        => !$acards || $runee1
-),
-'webr' => array(
-	'title'       => 'Webradio Import',
-	'maintainer'  => 'r e r n',
-	'description' => 'Webradio files import. Adding files to <code>/mnt/MPD/Webradio/</code> alone will not work.'
-					.'<br>Add files at anytime then run this addon to refresh Webradio list.'
-					.'<br><w>Webradio Sorting</w> should be installed after import on 0.3.',
-	'thumbnail'   => '/img/addons/thumbwebr.png',
-	'buttonlabel' => 'Import',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/webradio',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/webradio/webradiodb.sh',
-	'option'      => array(
-		'wait'      => 'Get webradio <code>*.pls</code> or <code>*.m3u</code> files or folders'
-					  .'<br>copied to <code>/mnt/MPD/Webradio</code>'
-	),
-	'hide'        => $enha || $runee1
-),
-'noti' => array(
-	'title'       => 'Setting - Notification Duration',
-	'maintainer'  => 'r e r n',
-	'description' => 'Change RuneUI notification duration',
-	'thumbnail'   => '/img/addons/thumbnoti.gif',
-	'buttonlabel' => 'Change',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/set_notify',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/set_notify/set.sh',
-	'option'      => array(
-		'radio'     => array(
-			'message' => 'Notification duration(seconds):',
-			'list'    => array(
-				'1'           => 1,
-				'2'           => 2,
-				'3'           => 3,
-				'4'           => 4,
-				'5'           => 5,
-				'6'           => 6,
-				'7'           => 7,
-				'8 (default)' => 8,
-				'Custom'      => '?'
-			),
-			'checked' => exec( 'grep setTimeout /srv/http/assets/js/enhancebanner.js | cut -d" " -f5' ) / 1000
-		),
-	),
-	'hide'        => $runee1
-),
-'zoom' => array(
-	'title'       => 'Setting - Local Browser - Zoom Level',
-	'maintainer'  => 'r e r n',
-	'description' => 'Change Zoom Level of local browser (for Midori and Chromium only)',
-	'buttonlabel' => 'Change',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/set_zoom',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/set_zoom/set.sh',
-	'option'      => array(
-		'radio'     => array(
-			'message' => 'Zoom level:',
-			'list'    => array(
-				'VGA or less - 720px: 0.7'   => 0.7,
-				'XGA - 1024px: 1.0'          => 1,
-				'SXGA - 1280px: 1.5'         => 1.5,
-				'Full HD - 1920px: 1.8'      => 1.8,
-				'Full HD - 1920px: 2.0'      => 2.0,
-				'Custom'                     => '?'
-			),
-			'checked' => $zoom,
-		),
-	),
-	'hide'        => !$localbrowser || $rune05 || $runee1
-),
-'poin' => array(
-	'title'       => 'Setting - Local Browser - Mouse Pointer',
-	'maintainer'  => 'r e r n',
-	'description' => 'Enable mouse pointer on local browser',
-	'buttonlabel' => 'Change',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/set_pointer',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/set_pointer/set.sh',
-	'option'      => array(
-		'radio'     => array(
-			'message' => 'Mouse pointer on local browser:',
-			'list'    => array(
-				'Enable'  => 'yes',
-				'Disable' => 'no',
-			),
-			'checked' => exec( "grep use_cursor /root/.xinitrc | sed 's/.*cursor \\(.*\\) &/\\1/'" ),
-		),
-	),
-	'hide'        => !$localbrowser || $rune05 || $runee1
-),
-'wifi' => array(
-	'title'       => 'Setting - On/Off WiFi and Bluetooth',
-	'maintainer'  => 'r e r n',
-	'description' => 'On/Off WiFi and Bluetooth',
-	'buttonlabel' => 'Change',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/set_wlan-bt',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/set_wlan-bt/set.sh',
-	'option'      => array(
-		'radio'     => array(
-			'message' => 'WiFi and Bluetooth:',
-			'list'    => array(
-				'Enable'  => 1,
-				'Disable' => 0,
-			),
-			'checked' => file_exists( '/etc/systemd/system/multi-user.target.wants/netctl-auto@wlan0.service' ),
-		),
-	),
-	'hide'        => $runee1
-),
-'soff' => array(
-	'title'       => 'Setting - Screen Off Timeout',
-	'maintainer'  => 'r e r n',
-	'description' => 'Set screen off timeout, <w>DPMS (Energy Star) standby</w>, for local browser.',
-	'buttonlabel' => 'Change',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/set_screenoff',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/set_screenoff/set.sh',
-	'option'      => array(
-		'radio'     => array(
-			'message' => 'Screen off timeout:',
-			'list'    => array(
-				'5 minutes'  => 5,
-				'10 minutes' => 10,
-				'15 minutes' => 15,
-				'Disable'    => 0,
-			),
-			'checked' => exec( "export DISPLAY=:0; xset q | grep Standby: | awk '{print $6}'" ) / 60
-		),
-	),
-	'hide'        => !$redis->get( 'local_browser' ) || $runee1
-),
 'back' => array(
 	'title'       => 'Settings+Databases Backup',
 	'revision'    => 'Initial release',
@@ -593,24 +248,6 @@ $addons = array(
 	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/backup-restore',
 	'option'      => array(
 		'confirm'   => 'Backup all RuneAudio <w>settings and databases</w>?',
-	),
-	'hide'        => $runee1
-),
-'rest' => array(
-	'title'       => 'Settings+Databases Restore',
-	'revision'    => 'Initial release',
-	'maintainer'  => 'r e r n',
-	'description' => 'Restore all RuneAudio <w>settings and databases</w>.',
-	'thumbnail'   => '/img/addons/thumbback.png',
-	'buttonlabel' => 'Restore',
-	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/backup-restore',
-	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/backup-restore/restore.sh',
-	'option'      => array(
-		'file'      => array(
-			'message' => 'Select a <code>*.tar.gz</code> backup file for restore:',
-			'label'   => 'Restore',
-			'type'    => '.tar.gz'
-		),
 	),
 	'hide'        => $runee1
 ),
