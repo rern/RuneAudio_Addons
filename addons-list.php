341bf6229d275a879ec4ac29fd96abb7965426fc
rern <rernrern@gmail.com>
2019-10-17 13:53:56 +0700

    Add files via upload

---
+++ addons-list.php
@@ -0,0 +1,142 @@
+<?php
+// temp - minor updates
+
+///////////////////////////////////////////////////////////////
+$addons = [
+
+'rre1' => [
+	'title'       => 'RuneAudio+R e1 *',
+	'version'     => '201910081',
+	'revision'    => 'Fix - Switches in setting pages not responding'
+					.'<br>Fix - NAS mount notification bug'
+					.'<br>Allow consume mode while others still on.'
+					.'<br>...'
+					.'<br>Fix - Update notification when Auto update enabled'
+					.'<br>Fix - System page layout on small screen.'
+					.'<br>...'
+					.'<br>Fix - Library tools show/hide not working.'
+					.'<br>Fix - Bookmark thumbnails cannot be created.'
+					.'<br>Fix - NAS mounts data in persistent settings.'
+					.'<br>Fix - GPIO menu listed without installed addon.'
+					.'<br>Fix - RPi browser current screenoff duration display in seconds.'
+					.'<br>Fix - I2S module "Generic AKM AK4xxx" cannot be set.',
+	'maintainer'  => 'r e r n',
+	'description' => 'Updates for RuneAudio <i class="fa fa-addons"></i> e1.',
+	'buttonlabel' => 'Update',
+	'nouninstall' => 1,
+	'thumbnail'   => '/img/addons/thumbenha.gif',
+	'sourcecode'  => 'https://github.com/rern/RuneAudio-Re1',
+	'installurl'  => 'https://github.com/rern/RuneAudio-Re1/raw/master/install.sh',
+],
+'extr' => [
+	'title'       => 'RuneAudio+R e1 - Restore Extra Directories',
+	'maintainer'  => 'r e r n',
+	'description' => 'Restore extra directories: bookmarks, coverarts, lyrics, playlists and webradios.',
+	'buttonlabel' => 'Restore',
+	'sourcecode'  => 'https://github.com/rern/RuneAudio/raw/master/extradir',
+	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/extradir/install.sh',
+	'hide'        => 1
+],
+'rrre' => [
+	'title'       => 'RuneAudio+R e1 -  Reset',
+	'maintainer'  => 'r e r n',
+	'description' => 'Reset RuneAudio <i class="fa fa-addons"></i> e1 for initial setup.',
+	'buttonlabel' => '<i class="fa fa-reset"></i>Reset',
+	'thumbnail'   => '/img/addons/thumbreset.png',
+	'sourcecode'  => 'https://github.com/rern/RuneAudio-Re1/raw/master/setup.sh',
+	'installurl'  => 'https://github.com/rern/RuneAudio-Re1/raw/master/setup.sh',
+],
+'radi' => [
+	'title'       => 'RuneAudio+R e1 - Import Webradio',
+	'maintainer'  => 'r e r n',
+	'description' => 'Import <code>*.pls</code> or <code>*.m3u</code> webradio files.',
+	'buttonlabel' => '<i class="fa fa-input"></i>Import',
+	'thumbnail'   => '/img/addons/thumbwebr.png',
+	'sourcecode'  => 'https://github.com/rern/RuneAudio-Re1/blob/master/importwebradio.sh',
+	'installurl'  => 'https://github.com/rern/RuneAudio-Re1/raw/master/importwebradio.sh',
+	'option'      => [
+		'wait'      => 'Copy directory with webradio files:'
+					  .'<br><code>Webradio/*</code> > <code>/mnt/MPD</code>'
+					  .'<br>before continue.'
+	],
+],
+'cove' => [
+	'title'       => 'Browse By CoverArt Thumbnails',
+	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/coverarts/scan.sh',
+	'hide'        => 1,
+],
+'aria' => [
+	'title'       => 'Aria2',
+	'version'     => '20170901',
+	'needspace'   => 15,
+	'revision'    => 'Initial release',
+	'maintainer'  => 'r e r n',
+	'description' => 'Download utility that supports HTTP(S], FTP, BitTorrent, and Metalink.'
+					.'<br>Pre-configured and ready to use.',
+	'thumbnail'   => '/img/addons/thumbaria.png',
+	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/aria2',
+	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/aria2/install.sh',
+],
+'rank' => [
+	'title'       => 'Rank Mirror Package Servers',
+	'maintainer'  => 'r e r n',
+	'description' => 'Fix package download errors caused by unreachable servers.'
+					.'<br>Rank mirror package servers by download speed and latency.',
+	'thumbnail'   => '/img/addons/thumbrank.png',
+	'buttonlabel' => '<i class="fa fa-bars"></i>Rank',
+	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/rankmirrors',
+	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/rankmirrors/rankmirrors.sh',
+	'option'      => [
+		'radio'     => [
+			'message' => 'Download test for each server(seconds):',
+			'list'    => [
+				'3'  => 3,
+				'4'  => 4,
+				'5'  => 5,
+				'6'  => 6,
+				'7'  => 7,
+				'8'  => 8,
+				'9'  => 9,
+			],
+			'checked' => 3,
+		],
+	],
+],
+'gpio' => [
+	'title'       => 'RuneUI GPIO',
+	'version'     => '20191006',
+	'needspace'   => 5,
+	'revision'    => 'Support RuneAudio+R e1'
+					.'<br>...'
+					.'<br>Link setting location to common directory'
+					.'<br>...'
+					.'<br>Improve notifications - show devices name',
+	'maintainer'  => 'r e r n',
+	'description' => 'GPIO-connected relay module control for power on / off audio equipments.',
+	'thumbnail'   => '/img/addons/thumbgpio.gif',
+	'sourcecode'  => 'https://github.com/rern/RuneUI_GPIO',
+	'installurl'  => 'https://github.com/rern/RuneUI_GPIO/raw/master/install.sh',
+],
+'tran' => [
+	'title'       => 'Transmission',
+	'version'     => '20190911',
+	'needspace'   => 9,
+	'revision'    => 'Support RuneAudio+R e1'
+					.'<br>...'
+					.'<br>Fix bugs by reverting back to custom compiled package.'
+					.'<br>...'
+					.'<br>Update alternateive WebUI source',
+	'maintainer'  => 'r e r n',
+	'description' => 'Fast, easy, and free BitTorrent client. Pre-configured and ready to use.',
+	'thumbnail'   => '/img/addons/thumbtran.png',
+	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/transmission',
+	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/transmission/install.sh',
+	'option'      => [
+		'password'  => [
+			'message' => 'Password for user <w>root</w> (blank = no password):',
+			'label'   => 'Password',
+		],
+	],
+],
+
+];
