ab1b1af4838e336a0259c276ddf0111004a8c409
rern <rernrern@gmail.com>
2019-11-11 14:53:48 +0700

    Update addons-list.php

--- addons-list.php
+++ addons-list.php
@@ -12,7 +12,7 @@ $addons = [
 	'description' => 'Updates for RuneAudio <i class="fa fa-addons"></i> e2.',
 	'buttonlabel' => 'Update',
 	'nouninstall' => 1,
-	'thumbnail'   => '/img/addons/thumbenha.gif',
+	'thumbnail'   => '/assets/img/addons/thumbenha.gif',
 	'sourcecode'  => 'https://github.com/rern/RuneAudio-Re2',
 	'installurl'  => 'https://github.com/rern/RuneAudio-Re2/raw/master/install.sh',
 	'hide'        => 1
@@ -22,6 +22,7 @@ $addons = [
 	'maintainer'  => 'r e r n',
 	'description' => 'Restore database and settings from backup.',
 	'buttonlabel' => 'Restore',
+	'thumbnail'   => '/assets/img/addons/thumbrest.gif',
 	'sourcecode'  => 'https://github.com/rern/RuneOS/raw/master/usr/local/bin/runerestore.sh',
 	'installurl'  => 'https://github.com/rern/RuneOS/raw/master/usr/local/bin/runerestore.sh',
 	'option'      => [
@@ -39,7 +40,7 @@ $addons = [
 	'title'       => 'Expand Partition',
 	'maintainer'  => 'r e r n',
 	'description' => 'Expand SD card ROOT partition to full capacity.',
-	'thumbnail'   => '/img/addons/thumbpart.png',
+	'thumbnail'   => '/assets/img/addons/thumbexpa.png',
 	'buttonlabel' => 'Expand',
 	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/expand_partition',
 	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/expand_partition/expand.sh',
@@ -50,7 +51,7 @@ $addons = [
 	'maintainer'  => 'r e r n',
 	'description' => 'Fix package download errors caused by unreachable servers.'
 					.'<br>Rank mirror package servers by download speed and latency.',
-	'thumbnail'   => '/img/addons/thumbrank.png',
+	'thumbnail'   => '/assets/img/addons/thumbrank.png',
 	'buttonlabel' => '<i class="fa fa-bars"></i>Rank',
 	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/rankmirrors',
 	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/rankmirrors/rankmirrors.sh',
@@ -81,7 +82,7 @@ $addons = [
 					.'<br>Improve notifications - show devices name',
 	'maintainer'  => 'r e r n',
 	'description' => 'GPIO-connected relay module control for power on / off audio equipments.',
-	'thumbnail'   => '/img/addons/thumbgpio.gif',
+	'thumbnail'   => '/assets/img/addons/thumbgpio.gif',
 	'sourcecode'  => 'https://github.com/rern/RuneUI_GPIO',
 	'installurl'  => 'https://github.com/rern/RuneUI_GPIO/raw/master/install.sh',
 ],
@@ -93,7 +94,7 @@ $addons = [
 	'maintainer'  => 'r e r n',
 	'description' => 'Download utility that supports HTTP(S], FTP, BitTorrent, and Metalink.'
 					.'<br>Pre-configured and ready to use.',
-	'thumbnail'   => '/img/addons/thumbaria.png',
+	'thumbnail'   => '/assets/img/addons/thumbaria.png',
 	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/aria2',
 	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/aria2/install.sh',
 ],
@@ -108,7 +109,7 @@ $addons = [
 					.'<br>Update alternateive WebUI source',
 	'maintainer'  => 'r e r n',
 	'description' => 'Fast, easy, and free BitTorrent client. Pre-configured and ready to use.',
-	'thumbnail'   => '/img/addons/thumbtran.png',
+	'thumbnail'   => '/assets/img/addons/thumbtran.png',
 	'sourcecode'  => 'https://github.com/rern/RuneAudio/tree/master/transmission',
 	'installurl'  => 'https://github.com/rern/RuneAudio/raw/master/transmission/install.sh',
 	'option'      => [
