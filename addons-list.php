820161c5714a23b8a62cb19143168844f68cf4f0
rern <rernrern@gmail.com>
2020-05-27 10:48:17 +0700

    Update addons-list.php

--- addons-list.php
+++ addons-list.php
@@ -6,7 +6,7 @@ $addons = [
 
 'rre3' => [
 	'title'       => 'RuneAudio+R e3',
-	'version'     => '20200525',
+	'version'     => '20200527',
 	'revision'    => 'Fix bugs',
 	'maintainer'  => 'r e r n',
 	'description' => 'Updates for RuneAudio <i class="fa fa-addons"></i> e3.',
