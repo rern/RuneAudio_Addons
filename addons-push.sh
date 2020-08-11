267b62b58e99e49005905c5aaabc0f5bbf06faeb
rern <rernrern@gmail.com>
2020-08-11 20:39:39 +0700

    Update addons-push.sh

--- addons-push.sh
+++ addons-push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://github.com/rern/RuneAudio-Re5/raw/master/srv/http/addons.php -qO /srv/http/addons.php
+[[ -e /srv/http/data/addons/rre4 ]] && wget https://github.com/rern/RuneAudio-Re5/raw/master/srv/http/addons.php -qO /srv/http/addons.php
