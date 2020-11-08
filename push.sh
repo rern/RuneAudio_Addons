37d234e7547171570ecb21ce3529a6ff07e4bb28
rern <rernrern@gmail.com>
2020-11-08 21:00:55 +0700

    u

--- push.sh
+++ push.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+[[ $( cat /srv/http/data/addons/rre6 ) > 20201107 ]] && exit
+
 sed -i 's|/addons.php|/settings&|' /srv/http/addons-progress.php &> /dev/null
 
-[[ -e /srv/http/addons.php ]] && file=/srv/http/addons.php || file=/srv/http/settings/addons.php
-
-if ! grep -q verify $file; then
-	wget -q https://github.com/rern/RuneAudio-Re6/raw/UPDATE/srv/http/settings/addons.php -O $file
-	wget -q https://github.com/rern/RuneAudio-Re6/raw/UPDATE/srv/http/assets/css/addons.css -O /srv/http/assets/css/addons.css
-fi
+wget -q https://github.com/rern/RuneAudio-Re6/raw/UPDATE/srv/http/assets/css/addons.css -O /srv/http/assets/css/addons.css
+wget -q https://github.com/rern/RuneAudio-Re6/raw/UPDATE/srv/http/settings/addons.php -O /srv/http/settings/addons.php
+wget -q https://github.com/rern/RuneAudio-Re6/raw/UPDATE/srv/http/settings/addons-progress.php -O /srv/http/settings/addons-progress.php
