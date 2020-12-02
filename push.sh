0ff958ab94844f916acc86545fb6fae8637606c7
rern <rernrern@gmail.com>
2020-12-02 10:48:50 +0700

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-[[ $( cat /srv/http/data/addons/rre6 ) > 20201107 ]] && exit
-
-sed -i 's|/addons.php|/settings&|' /srv/http/addons-progress.php &> /dev/null
-
-wget -q https://github.com/rern/RuneAudio-Re6/raw/UPDATE/srv/http/assets/css/addons.css -O /srv/http/assets/css/addons.css
-wget -q https://github.com/rern/RuneAudio-Re6/raw/UPDATE/srv/http/settings/addons.php -O /srv/http/settings/addons.php
-wget -q https://github.com/rern/RuneAudio-Re6/raw/UPDATE/srv/http/settings/addons-progress.php -O /srv/http/settings/addons-progress.php
+sed i 's/rpi=1$/&;;/' /srv/http/bash/datareset.sh
